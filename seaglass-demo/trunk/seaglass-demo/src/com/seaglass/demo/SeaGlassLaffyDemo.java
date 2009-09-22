e3ff0f8ac881447d66261e75498253879578334d
kathryn <kathryn@kathrynhuxtable.org>
2009-09-22 23:33:39 +0000

    Use Laffy as base for our demo. Install SeaGlass and then call Laffy's main method. Need to figure out how to make SeaGlass the default L&F.

---
+++ seaglass-demo/trunk/seaglass-demo/src/com/seaglass/demo/SeaGlassLaffyDemo.java
@@ -0,0 +1,33 @@
+package com.seaglass.demo;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+import org.jdesktop.laffy.Laffy;
+
+public class SeaGlassLaffyDemo {
+
+    public static void main(String[] args) throws Exception {
+        // Set look and feel
+        SwingUtilities.invokeAndWait(new Runnable() {
+            public void run() {
+                // set SeaGlass laf if available
+                try {
+                    UIManager.installLookAndFeel("SeaGlass", "com.seaglass.SeaGlassLookAndFeel");
+                    UIManager.setLookAndFeel("com.seaglass.SeaGlassLookAndFeel");
+                } catch (Exception e) {
+                    System.err.println("Nimbus LAF not available using Ocean.");
+                    try {
+                        UIManager.setLookAndFeel(new MetalLookAndFeel());
+                    } catch (Exception e2) {
+                        System.err.println("Unable to use Ocean LAF using default.");
+                    }
+                }
+            }
+        });
+
+        // create laffy and load
+        Laffy.main(args);
+    }
+}
