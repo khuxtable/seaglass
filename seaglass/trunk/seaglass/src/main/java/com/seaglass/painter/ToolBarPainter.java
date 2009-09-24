74de371229a601135bd4c4eb9a201f8bee916351
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2009-09-24 06:05:43 +0000

    Use the Synth toolbar UI, but use a painter based on Ken's unified toolbar gradient.

--- seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/ToolBarPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/ToolBarPainter.java
@@ -22,35 +22,67 @@
 package com.seaglass.painter;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
 
 import com.seaglass.util.PlatformUtils;
 import com.seaglass.util.WindowUtils;
 
 /**
  * @author Ken Orr
+ * @author Modified by Kathryn Huxtable for SeaGlass
  */
-public class ToolBarPainter implements Painter<Component> {
-    final boolean IS_MAC_OSX                     = PlatformUtils.isMac();
+public class ToolBarPainter extends AbstractRegionPainter {
+    final boolean           IS_MAC_OSX                     = PlatformUtils.isMac();
+
+    // package private integers representing the available states that
+    // this painter will paint. These are used when creating a new instance
+    // of ToolBarPainter to determine which region/state is being painted
+    // by that instance.
+    public static final int BORDER_NORTH                   = 1;
+    public static final int BORDER_SOUTH                   = 2;
+    public static final int BORDER_EAST                    = 3;
+    public static final int BORDER_WEST                    = 4;
+    public static final int HANDLEICON_ENABLED             = 5;
 
     // For non-Mac use Snow Leopard colors because it has the same Gamma
     // correction.
-    private Color ACTIVE_TOP_GRADIENT_COLOR      = IS_MAC_OSX ? new Color(0xbcbcbc) : new Color(0xc4c4c4);
-    private Color ACTIVE_BOTTOM_GRADIENT_COLOR   = IS_MAC_OSX ? new Color(0x9a9a9a) : new Color(0xb2b2b2);
-    private Color INACTIVE_TOP_GRADIENT_COLOR    = IS_MAC_OSX ? new Color(0xe4e4e4) : new Color(0xe7e7e7);
-    private Color INACTIVE_BOTTOM_GRADIENT_COLOR = IS_MAC_OSX ? new Color(0xd1d1d1) : new Color(0xdfdfdf);
+    private Color           ACTIVE_TOP_GRADIENT_COLOR      = IS_MAC_OSX ? new Color(0xbcbcbc) : new Color(0xc4c4c4);
+    private Color           ACTIVE_BOTTOM_GRADIENT_COLOR   = IS_MAC_OSX ? new Color(0x9a9a9a) : new Color(0xb2b2b2);
+    private Color           INACTIVE_TOP_GRADIENT_COLOR    = IS_MAC_OSX ? new Color(0xe4e4e4) : new Color(0xe7e7e7);
+    private Color           INACTIVE_BOTTOM_GRADIENT_COLOR = IS_MAC_OSX ? new Color(0xd1d1d1) : new Color(0xdfdfdf);
 
-    public void paint(Graphics2D graphics2D, Component component, int width, int height) {
-        boolean containedInActiveWindow = WindowUtils.isParentWindowFocused(component);
+    // Refers to one of the static final ints above
+    private int             state;
+    private PaintContext    ctx;
+
+    public ToolBarPainter(PaintContext ctx, int state) {
+        super();
+        this.state = state;
+        this.ctx = ctx;
+    }
+
+    @Override
+    protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
+        boolean containedInActiveWindow = WindowUtils.isParentWindowFocused(c);
 
         Color topColor = containedInActiveWindow ? ACTIVE_TOP_GRADIENT_COLOR : INACTIVE_TOP_GRADIENT_COLOR;
         Color bottomColor = containedInActiveWindow ? ACTIVE_BOTTOM_GRADIENT_COLOR : INACTIVE_BOTTOM_GRADIENT_COLOR;
 
-        GradientPaint paint = new GradientPaint(0, 1, topColor, 0, height, bottomColor);
-        graphics2D.setPaint(paint);
-        graphics2D.fillRect(0, 0, width, height);
+        GradientPaint paint;
+        if (state == BORDER_EAST || state == BORDER_WEST) {
+            paint = new GradientPaint(1, 0, topColor, width, 0, bottomColor);
+        } else {
+            paint = new GradientPaint(0, 1, topColor, 0, height, bottomColor);
+        }
+        g.setPaint(paint);
+        g.fillRect(0, 0, width, height);
     }
 
+    @Override
+    protected PaintContext getPaintContext() {
+        return ctx;
+    }
 }
