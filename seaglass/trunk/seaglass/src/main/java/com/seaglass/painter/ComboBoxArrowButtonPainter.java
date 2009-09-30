f6ed64bac5fdbd5bd56215fd655b3e22b677944e
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2009-09-30 22:08:31 +0000

    Added code to use the dropdown arrow on editable combo boxes.

--- seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/ComboBoxArrowButtonPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/ComboBoxArrowButtonPainter.java
@@ -6,28 +6,30 @@
  */
 package com.seaglass.painter;
 
-
 /**
  */
 public final class ComboBoxArrowButtonPainter extends AbstractImagePainter {
-    //package private integers representing the available states that
-    //this painter will paint. These are used when creating a new instance
-    //of ComboBoxArrowButtonPainter to determine which region/state is being painted
-    //by that instance.
-    public static final int BACKGROUND_DISABLED = 1;
-    public static final int BACKGROUND_ENABLED = 2;
-    public static final int BACKGROUND_ENABLED_MOUSEOVER = 3;
-    public static final int BACKGROUND_ENABLED_PRESSED = 4;
-    public static final int BACKGROUND_DISABLED_EDITABLE = 5;
-    public static final int BACKGROUND_ENABLED_EDITABLE = 6;
+    // package private integers representing the available states that
+    // this painter will paint. These are used when creating a new instance
+    // of ComboBoxArrowButtonPainter to determine which region/state is being
+    // painted
+    // by that instance.
+    public static final int BACKGROUND_DISABLED           = 1;
+    public static final int BACKGROUND_ENABLED            = 2;
+    public static final int BACKGROUND_ENABLED_MOUSEOVER  = 3;
+    public static final int BACKGROUND_ENABLED_PRESSED    = 4;
+    public static final int BACKGROUND_DISABLED_EDITABLE  = 5;
+    public static final int BACKGROUND_ENABLED_EDITABLE   = 6;
     public static final int BACKGROUND_MOUSEOVER_EDITABLE = 7;
-    public static final int BACKGROUND_PRESSED_EDITABLE = 8;
-    public static final int BACKGROUND_SELECTED_EDITABLE = 9;
-    public static final int FOREGROUND_ENABLED = 10;
-    public static final int FOREGROUND_MOUSEOVER = 11;
-    public static final int FOREGROUND_DISABLED = 12;
-    public static final int FOREGROUND_PRESSED = 13;
-    public static final int FOREGROUND_SELECTED = 14;
+    public static final int BACKGROUND_PRESSED_EDITABLE   = 8;
+    public static final int BACKGROUND_SELECTED_EDITABLE  = 9;
+    public static final int FOREGROUND_ENABLED            = 10;
+    public static final int FOREGROUND_MOUSEOVER          = 11;
+    public static final int FOREGROUND_DISABLED           = 12;
+    public static final int FOREGROUND_PRESSED            = 13;
+    public static final int FOREGROUND_SELECTED           = 14;
+    public static final int FOREGROUND_EDITABLE           = 15;
+    public static final int FOREGROUND_EDITABLE_DISABLED  = 16;
 
     public ComboBoxArrowButtonPainter(PaintContext ctx, int state) {
         super(ctx, state);
@@ -35,17 +37,31 @@ public final class ComboBoxArrowButtonPainter extends AbstractImagePainter {
 
     @Override
     protected String getImageName(int state) {
-        switch(state) {
-            case BACKGROUND_DISABLED_EDITABLE: return "combo_box_button_background_disabled";
-            case BACKGROUND_ENABLED_EDITABLE: return "combo_box_button_background";
-            case BACKGROUND_MOUSEOVER_EDITABLE: return "combo_box_button_background";
-            case BACKGROUND_PRESSED_EDITABLE: return "combo_box_button_background_pressed";
-            case BACKGROUND_SELECTED_EDITABLE: return "combo_box_button_background_pressed";
-            case FOREGROUND_ENABLED: return "combo_box_arrows";
-            case FOREGROUND_MOUSEOVER: return "combo_box_arrows";
-            case FOREGROUND_DISABLED: return "combo_box_arrow_foreground_disabled";
-            case FOREGROUND_PRESSED: return "combo_box_arrows";
-            case FOREGROUND_SELECTED: return "combo_box_arrows";
+        switch (state) {
+        case BACKGROUND_DISABLED_EDITABLE:
+            return "combo_box_button_background_disabled";
+        case BACKGROUND_ENABLED_EDITABLE:
+            return "combo_box_button_background";
+        case BACKGROUND_MOUSEOVER_EDITABLE:
+            return "combo_box_button_background";
+        case BACKGROUND_PRESSED_EDITABLE:
+            return "combo_box_button_background_pressed";
+        case BACKGROUND_SELECTED_EDITABLE:
+            return "combo_box_button_background_pressed";
+        case FOREGROUND_ENABLED:
+            return "combo_box_arrows";
+        case FOREGROUND_MOUSEOVER:
+            return "combo_box_arrows";
+        case FOREGROUND_DISABLED:
+            return "combo_box_arrows_disabled";
+        case FOREGROUND_PRESSED:
+            return "combo_box_arrows";
+        case FOREGROUND_SELECTED:
+            return "combo_box_arrows";
+        case FOREGROUND_EDITABLE:
+            return "combo_box_arrow_dropdown";
+        case FOREGROUND_EDITABLE_DISABLED:
+            return "combo_box_arrow_dropdown_disabled";
         }
         return null;
     }
