1eba4dd1a165e4dd4f5b2ed97cf3049335886d6f
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2009-10-07 00:45:26 +0000

    Fiddled with some code copied from Nimbus.

--- seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/PopupMenuPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/PopupMenuPainter.java
@@ -33,26 +33,17 @@ import javax.swing.JComponent;
  * Based on Nimbus's implementation.
  */
 public final class PopupMenuPainter extends AbstractRegionPainter {
-    //package private integers representing the available states that
-    //this painter will paint. These are used when creating a new instance
-    //of PopupMenuPainter to determine which region/state is being painted
-    //by that instance.
     public static final int BACKGROUND_DISABLED = 1;
-    public static final int BACKGROUND_ENABLED = 2;
+    public static final int BACKGROUND_ENABLED  = 2;
 
+    private int             state;
+    private PaintContext    ctx;
 
-    private int state; //refers to one of the static final ints above
-    private PaintContext ctx;
+    private Rectangle2D     rect                = new Rectangle2D.Float(0, 0, 0, 0);
 
-    //the following 4 variables are reused during the painting code of the layers
-    private Rectangle2D rect = new Rectangle2D.Float(0, 0, 0, 0);
-
-    //All Colors used for painting are stored here. Ideally, only those colors being used
-    //by a particular instance of PopupMenuPainter would be created. For the moment at least,
-    //however, all are created for each instance.
-    private Color color1 = decodeColor("nimbusBlueGrey", -0.6111111f, -0.110526316f, -0.39607844f, 0);
-    private Color color2 = decodeColor("nimbusBase", 0.0f, -0.6357143f, 0.45098037f, 0);
-    private Color color3 = decodeColor("nimbusBase", 0.021348298f, -0.6150531f, 0.39999998f, 0);
+    private Color           color1              = decodeColor("nimbusBlueGrey", -0.6111111f, -0.110526316f, -0.39607844f, 0);
+    private Color           color2              = decodeColor("nimbusBase", 0.0f, -0.6357143f, 0.45098037f, 0);
+    private Color           color3              = decodeColor("nimbusBase", 0.021348298f, -0.6150531f, 0.39999998f, 0);
 
     public PopupMenuPainter(PaintContext ctx, int state) {
         super();
@@ -60,17 +51,17 @@ public final class PopupMenuPainter extends AbstractRegionPainter {
         this.ctx = ctx;
     }
 
-    @Override
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
-        //generate this entire method. Each state/bg/fg/border combo that has
-        //been painted gets its own KEY and paint method.
-        switch(state) {
-            case BACKGROUND_DISABLED: paintBackgroundDisabled(g); break;
-            case BACKGROUND_ENABLED: paintBackgroundEnabled(g); break;
+        switch (state) {
+        case BACKGROUND_DISABLED:
+            paintBackgroundDisabled(g);
+            break;
+        case BACKGROUND_ENABLED:
+            paintBackgroundEnabled(g);
+            break;
         }
     }
 
-    @Override
     protected final PaintContext getPaintContext() {
         return ctx;
     }
@@ -96,51 +87,57 @@ public final class PopupMenuPainter extends AbstractRegionPainter {
     }
 
     private Rectangle2D decodeRect1() {
-            rect.setRect(decodeX(1.0f), //x
-                         decodeY(0.0f), //y
-                         decodeX(2.0f) - decodeX(1.0f), //width
-                         decodeY(3.0f) - decodeY(0.0f)); //height
+        rect.setRect(decodeX(1.0f), // x
+            decodeY(0.0f), // y
+            decodeX(2.0f) - decodeX(1.0f), // width
+            decodeY(3.0f) - decodeY(0.0f)); // height
         return rect;
     }
 
     private Rectangle2D decodeRect2() {
-            rect.setRect(decodeX(1.0045455f), //x
-                         decodeY(0.11111111f), //y
-                         decodeX(1.9954545f) - decodeX(1.0045455f), //width
-                         decodeY(2.909091f) - decodeY(0.11111111f)); //height
+        rect.setRect(decodeX(1.0045455f), // x
+            decodeY(0.11111111f), // y
+            decodeX(1.9954545f) - decodeX(1.0045455f), // width
+            decodeY(2.909091f) - decodeY(0.11111111f)); // height
         return rect;
     }
 
     private Rectangle2D decodeRect3() {
-            rect.setRect(decodeX(0.0f), //x
-                         decodeY(0.0f), //y
-                         decodeX(3.0f) - decodeX(0.0f), //width
-                         decodeY(3.0f) - decodeY(0.0f)); //height
+        rect.setRect(decodeX(0.0f), // x
+            decodeY(0.0f), // y
+            decodeX(3.0f) - decodeX(0.0f), // width
+            decodeY(3.0f) - decodeY(0.0f)); // height
         return rect;
     }
 
     private Rectangle2D decodeRect4() {
-            rect.setRect(decodeX(0.5f), //x
-                         decodeY(0.09090909f), //y
-                         decodeX(2.5f) - decodeX(0.5f), //width
-                         decodeY(2.909091f) - decodeY(0.09090909f)); //height
+        rect.setRect(decodeX(0.5f), // x
+            decodeY(0.09090909f), // y
+            decodeX(2.5f) - decodeX(0.5f), // width
+            decodeY(2.909091f) - decodeY(0.09090909f)); // height
         return rect;
     }
 
     private Paint decodeGradient1(Shape s) {
         Rectangle2D bounds = s.getBounds2D();
-        float x = (float)bounds.getX();
-        float y = (float)bounds.getY();
-        float w = (float)bounds.getWidth();
-        float h = (float)bounds.getHeight();
-        return decodeGradient((0.5f * w) + x, (0.0f * h) + y, (0.5f * w) + x, (1.0f * h) + y,
-                new float[] { 0.0f,0.0030f,0.02f,0.5f,0.98f,0.996f,1.0f },
-                new Color[] { color2,
-                            decodeColor(color2,color3,0.5f),
-                            color3,
-                            decodeColor(color3,color3,0.5f),
-                            color3,
-                            decodeColor(color3,color2,0.5f),
-                            color2});
+        float x = (float) bounds.getX();
+        float y = (float) bounds.getY();
+        float w = (float) bounds.getWidth();
+        float h = (float) bounds.getHeight();
+        return decodeGradient((0.5f * w) + x, (0.0f * h) + y, (0.5f * w) + x, (1.0f * h) + y, new float[] {
+            0.0f,
+            0.0030f,
+            0.02f,
+            0.5f,
+            0.98f,
+            0.996f,
+            1.0f }, new Color[] {
+            color2,
+            decodeColor(color2, color3, 0.5f),
+            color3,
+            decodeColor(color3, color3, 0.5f),
+            color3,
+            decodeColor(color3, color2, 0.5f),
+            color2 });
     }
 }
--- seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/PopupMenuSeparatorPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/PopupMenuSeparatorPainter.java
@@ -31,23 +31,14 @@ import javax.swing.JComponent;
  * Based on Nimbus's PopupMenuSepatorPainter.
  */
 public final class PopupMenuSeparatorPainter extends AbstractRegionPainter {
-    //package private integers representing the available states that
-    //this painter will paint. These are used when creating a new instance
-    //of PopupMenuSeparatorPainter to determine which region/state is being painted
-    //by that instance.
     public static final int BACKGROUND_ENABLED = 1;
 
+    private int             state;
+    private PaintContext    ctx;
 
-    private int state; //refers to one of the static final ints above
-    private PaintContext ctx;
+    private Rectangle2D     rect               = new Rectangle2D.Float(0, 0, 0, 0);
 
-    //the following 4 variables are reused during the painting code of the layers
-    private Rectangle2D rect = new Rectangle2D.Float(0, 0, 0, 0);
-
-    //All Colors used for painting are stored here. Ideally, only those colors being used
-    //by a particular instance of PopupMenuSeparatorPainter would be created. For the moment at least,
-    //however, all are created for each instance.
-    private Color color1 = decodeColor("nimbusBlueGrey", -0.008547008f, -0.03830409f, -0.039215684f, 0);
+    private Color           color1             = decodeColor("nimbusBlueGrey", -0.008547008f, -0.03830409f, -0.039215684f, 0);
 
     public PopupMenuSeparatorPainter(PaintContext ctx, int state) {
         super();
@@ -57,13 +48,13 @@ public final class PopupMenuSeparatorPainter extends AbstractRegionPainter {
 
     @Override
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
-        //generate this entire method. Each state/bg/fg/border combo that has
-        //been painted gets its own KEY and paint method.
-        switch(state) {
-            case BACKGROUND_ENABLED: paintBackgroundEnabled(g); break;
+        switch (state) {
+        case BACKGROUND_ENABLED:
+            paintBackgroundEnabled(g);
+            break;
         }
     }
-        
+
     @Override
     protected final PaintContext getPaintContext() {
         return ctx;
@@ -77,10 +68,10 @@ public final class PopupMenuSeparatorPainter extends AbstractRegionPainter {
     }
 
     private Rectangle2D decodeRect1() {
-            rect.setRect(decodeX(0.0f), //x
-                         decodeY(1.0f), //y
-                         decodeX(3.0f) - decodeX(0.0f), //width
-                         decodeY(2.0f) - decodeY(1.0f)); //height
+        rect.setRect(decodeX(0.0f), // x
+            decodeY(1.0f), // y
+            decodeX(3.0f) - decodeX(0.0f), // width
+            decodeY(2.0f) - decodeY(1.0f)); // height
         return rect;
     }
 }
