a99312a59afeb759fcdbe36272deb50c1d692e73
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2009-10-09 05:42:39 +0000

    Added painters for text fields.

---
+++ seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/TextFieldPainter.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2009 Kathryn Huxtable and Kenneth Orr.
+ *
+ * This file is part of the SeaGlass Pluggable Look and Feel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id$
+ */
+package com.seaglass.painter;
+
+/**
+ */
+public final class TextFieldPainter extends AbstractImagePainter {
+    public static final int BACKGROUND_DISABLED = 1;
+    public static final int BACKGROUND_ENABLED  = 2;
+    public static final int BACKGROUND_SELECTED = 3;
+    public static final int BORDER_DISABLED     = 4;
+    public static final int BORDER_FOCUSED      = 5;
+    public static final int BORDER_ENABLED      = 6;
+
+    public TextFieldPainter(PaintContext ctx, int state) {
+        super(ctx, state);
+    }
+
+    protected String getImageName(int state) {
+        switch (state) {
+        case BACKGROUND_DISABLED:
+            return "textfield_disabled";
+        case BACKGROUND_ENABLED:
+            return "textfield";
+        case BACKGROUND_SELECTED:
+            return "textfield";
+        case BORDER_DISABLED:
+            return "textfield_border_disabled";
+        case BORDER_FOCUSED:
+            return "textfield_border_focused";
+        case BORDER_ENABLED:
+            return "textfield_border";
+        }
+        return null;
+    }
+}
