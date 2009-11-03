91428c4360cf380cf7b314b8cd77a311abe6ea33
kathryn <kathryn@kathrynhuxtable.org>
2009-11-03 15:52:26 +0000

    Updated to handle enabled/disabled states in the Nimbus fashion. Use the Nimbus painter for the drag handle for now.

--- seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/ToolBarPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/ToolBarPainter.java
@@ -22,14 +22,19 @@ package com.seaglass.painter;
 import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Shape;
+import java.awt.geom.Path2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JComponent;
 
 import com.seaglass.util.PlatformUtils;
-import com.seaglass.util.WindowUtils;
 
 /**
  * ToolBarPainter implementation.
+ * 
+ * Parts taken from Nimbus to draw drag handle.
  * 
  * @author Ken Orr
  * @author Modified by Kathryn Huxtable for SeaGlass
@@ -41,7 +46,11 @@ public class ToolBarPainter extends AbstractRegionPainter {
     public static final int BORDER_SOUTH                   = 2;
     public static final int BORDER_EAST                    = 3;
     public static final int BORDER_WEST                    = 4;
-    public static final int HANDLEICON_ENABLED             = 5;
+    public static final int BORDER_NORTH_ENABLED           = 6;
+    public static final int BORDER_SOUTH_ENABLED           = 7;
+    public static final int BORDER_EAST_ENABLED            = 8;
+    public static final int BORDER_WEST_ENABLED            = 9;
+    public static final int HANDLEICON_ENABLED             = 10;
 
     // For non-Mac use Snow Leopard colors because it has the same Gamma
     // correction.
@@ -61,22 +70,124 @@ public class ToolBarPainter extends AbstractRegionPainter {
     }
 
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
-        boolean containedInActiveWindow = WindowUtils.isParentWindowFocused(c);
-
-        Color topColor = containedInActiveWindow ? ACTIVE_TOP_GRADIENT_COLOR : INACTIVE_TOP_GRADIENT_COLOR;
-        Color bottomColor = containedInActiveWindow ? ACTIVE_BOTTOM_GRADIENT_COLOR : INACTIVE_BOTTOM_GRADIENT_COLOR;
-
-        GradientPaint paint;
-        if (state == BORDER_EAST || state == BORDER_WEST) {
-            paint = new GradientPaint(1, 0, topColor, width, 0, bottomColor);
-        } else {
-            paint = new GradientPaint(0, 1, topColor, 0, height, bottomColor);
+        if (state == HANDLEICON_ENABLED) {
+            painthandleIconEnabled(g);
+            return;
         }
+        
+        Color topColor = null;
+        Color bottomColor = null;
+        switch (state) {
+        case BORDER_NORTH:
+        case BORDER_SOUTH:
+        case BORDER_EAST:
+        case BORDER_WEST:
+            topColor = INACTIVE_TOP_GRADIENT_COLOR;
+            bottomColor = INACTIVE_BOTTOM_GRADIENT_COLOR;
+            break;
+        case BORDER_NORTH_ENABLED:
+        case BORDER_SOUTH_ENABLED:
+        case BORDER_EAST_ENABLED:
+        case BORDER_WEST_ENABLED:
+            topColor = ACTIVE_TOP_GRADIENT_COLOR;
+            bottomColor = ACTIVE_BOTTOM_GRADIENT_COLOR;
+            break;
+        }
+
+        GradientPaint paint = null;
+        switch (state) {
+        case BORDER_NORTH:
+        case BORDER_NORTH_ENABLED:
+            paint = new GradientPaint(0, 1, topColor, 0, height, bottomColor);
+            break;
+        case BORDER_SOUTH:
+        case BORDER_SOUTH_ENABLED:
+            paint = new GradientPaint(0, 1, topColor, 0, height, bottomColor);
+            break;
+        case BORDER_EAST:
+        case BORDER_EAST_ENABLED:
+            paint = new GradientPaint(1, 0, topColor, width, 0, bottomColor);
+            break;
+        case BORDER_WEST:
+        case BORDER_WEST_ENABLED:
+            paint = new GradientPaint(1, 0, topColor, width, 0, bottomColor);
+            break;
+        }
+
         g.setPaint(paint);
         g.fillRect(0, 0, width, height);
     }
 
     protected PaintContext getPaintContext() {
         return ctx;
+    }
+
+    private Path2D      path   = new Path2D.Float();
+    private Rectangle2D rect   = new Rectangle2D.Float(0, 0, 0, 0);
+
+    private Color       color2 = decodeColor("nimbusBlueGrey", 0.0f, -0.110526316f, 0.25490195f, 0);
+    private Color       color3 = decodeColor("nimbusBlueGrey", -0.006944418f, -0.07399663f, 0.11372548f, 0);
+    private Color       color4 = decodeColor("nimbusBorder", 0.0f, -0.029675633f, 0.109803915f, 0);
+    private Color       color5 = decodeColor("nimbusBlueGrey", -0.008547008f, -0.03494492f, -0.07058823f, 0);
+
+    private void painthandleIconEnabled(Graphics2D g) {
+        rect = decodeRect3();
+        g.setPaint(decodeGradient1(rect));
+        g.fill(rect);
+        rect = decodeRect4();
+        g.setPaint(color4);
+        g.fill(rect);
+        path = decodePath1();
+        g.setPaint(color5);
+        g.fill(path);
+        path = decodePath2();
+        g.setPaint(color5);
+        g.fill(path);
+    }
+
+    private Rectangle2D decodeRect3() {
+        rect.setRect(decodeX(0.0f), // x
+            decodeY(0.0f), // y
+            decodeX(2.8f) - decodeX(0.0f), // width
+            decodeY(3.0f) - decodeY(0.0f)); // height
+        return rect;
+    }
+
+    private Rectangle2D decodeRect4() {
+        rect.setRect(decodeX(2.8f), // x
+            decodeY(0.0f), // y
+            decodeX(3.0f) - decodeX(2.8f), // width
+            decodeY(3.0f) - decodeY(0.0f)); // height
+        return rect;
+    }
+
+    private Path2D decodePath1() {
+        path.reset();
+        path.moveTo(decodeX(0.0f), decodeY(0.0f));
+        path.lineTo(decodeX(0.0f), decodeY(0.4f));
+        path.lineTo(decodeX(0.4f), decodeY(0.0f));
+        path.lineTo(decodeX(0.0f), decodeY(0.0f));
+        path.closePath();
+        return path;
+    }
+
+    private Path2D decodePath2() {
+        path.reset();
+        path.moveTo(decodeX(0.0f), decodeY(3.0f));
+        path.lineTo(decodeX(0.0f), decodeY(2.6f));
+        path.lineTo(decodeX(0.4f), decodeY(3.0f));
+        path.lineTo(decodeX(0.0f), decodeY(3.0f));
+        path.closePath();
+        return path;
+    }
+
+    private Paint decodeGradient1(Shape s) {
+        Rectangle2D bounds = s.getBounds2D();
+        float x = (float) bounds.getX();
+        float y = (float) bounds.getY();
+        float w = (float) bounds.getWidth();
+        float h = (float) bounds.getHeight();
+        return decodeGradient((0.0f * w) + x, (0.5f * h) + y, (1.0f * w) + x, (0.5f * h) + y, new float[] { 0.0f, 0.5f, 1.0f },
+            new Color[] { color2, decodeColor(color2, color3, 0.5f), color3 });
     }
 }
