468b41f28935814029d4e9cc39c790fbf87b9433
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2009-11-30 23:11:10 +0000

    Added a class comment.

--- seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/DesktopPanePainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglass/painter/DesktopPanePainter.java
@@ -33,6 +33,7 @@ import javax.swing.JComponent;
 import com.seaglass.painter.AbstractRegionPainter.PaintContext.CacheMode;
 
 /**
+ * Nimbus's DesktopPanePainter.
  */
 public final class DesktopPanePainter extends AbstractRegionPainter {
     public static enum Which {
