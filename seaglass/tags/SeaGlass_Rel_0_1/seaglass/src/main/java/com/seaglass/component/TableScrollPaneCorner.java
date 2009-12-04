4ddc7ffdfa0d69c9ff21eaba1f41cb8dee4d670b
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2009-12-04 14:47:27 +0000

    First released version (0.1).
    Tabbed pane artwork is still Nimbus and there are a lot of rough edges. Focus indications aren't present for most controls.

---
+++ seaglass/tags/SeaGlass_Rel_0_1/seaglass/src/main/java/com/seaglass/component/TableScrollPaneCorner.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2009 Kathryn Huxtable and Kenneth Orr.
+ *
+ * This file is part of the SeaGlass Pluggable Look and Feel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id$
+ */
+package com.seaglass.component;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
+import javax.swing.UIManager;
+import javax.swing.plaf.UIResource;
+
+import com.seaglass.painter.Painter;
+
+/**
+ * TableScrollPaneCorner - A simple component that paints itself using the table
+ * header background painter. It is used to fill the top right corner of
+ * scrollpane.
+ * 
+ * Based on Nimbus's TableScrollPaneCorner by Jasper Potts
+ * 
+ * @see com.sun.java.swing.plaf.nimbus.TableScrollPaneCorner
+ */
+public class TableScrollPaneCorner extends JComponent implements UIResource {
+
+    /**
+     * Paint the component using the Nimbus Table Header Background Painter
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        Painter painter = (Painter) UIManager.get("TableHeader:\"TableHeader.renderer\"[Enabled].backgroundPainter");
+        if (painter != null) {
+
+            Graphics2D g2d = (Graphics2D) g;
+            painter.paint(g2d, this, getWidth() + 1, getHeight());
+        }
+    }
+}
