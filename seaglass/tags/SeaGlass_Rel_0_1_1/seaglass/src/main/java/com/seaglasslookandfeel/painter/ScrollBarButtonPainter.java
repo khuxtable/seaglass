ba093749db557b618e396ecb3d05df7c6722b32c
kathryn <kathryn@kathrynhuxtable.org>
2009-12-07 01:55:16 +0000

    Release 0.1.1, which is almost identical to 0.1 except that the base package changed from com.seaglass to com.seaglasslookandfeel. The Maven groupId and artifactId were changed accordingly.

---
+++ seaglass/tags/SeaGlass_Rel_0_1_1/seaglass/src/main/java/com/seaglasslookandfeel/painter/ScrollBarButtonPainter.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2009 Kathryn Huxtable and Kenneth Orr.
+ *
+ * This file is part of the SeaGlass Pluggable Look and Feel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id$
+ */
+package com.seaglasslookandfeel.painter;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JScrollBar;
+
+import com.seaglasslookandfeel.SeaGlassLookAndFeel;
+import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
+import com.seaglasslookandfeel.ui.SeaGlassScrollBarUI;
+
+/**
+ * ScrollBarButtonPainter implementation.
+ */
+public final class ScrollBarButtonPainter extends AbstractImagePainter<ScrollBarButtonPainter.Which> {
+    public static enum Which {
+        FOREGROUND_ENABLED, FOREGROUND_DISABLED, FOREGROUND_MOUSEOVER, FOREGROUND_PRESSED,
+    }
+
+    private static final Insets    insets    = new Insets(1, 1, 1, 1);
+    private static final Dimension dimension = new Dimension(20, 15);
+    private static final CacheMode cacheMode = CacheMode.FIXED_SIZES;
+    private static final Double    maxH      = 1.0;
+    private static final Double    maxV      = 1.0;
+
+    ImageIcon                      rightImage;
+
+    public ScrollBarButtonPainter(Which state) {
+        super(state);
+        setPaintContext(new PaintContext(insets, dimension, false, cacheMode, maxH, maxV));
+    }
+
+    @Override
+    protected String getImageName(Which state) {
+        String leftName = null;
+        String rightName = null;
+        switch (state) {
+        case FOREGROUND_ENABLED:
+            leftName = "h_scroll_left_apart_enabled";
+            rightName = "h_scroll_right_apart_enabled";
+            break;
+        case FOREGROUND_DISABLED:
+            leftName = "h_scroll_left_apart_enabled";
+            rightName = "h_scroll_right_apart_enabled";
+            break;
+        case FOREGROUND_MOUSEOVER:
+            leftName = "h_scroll_left_apart_enabled";
+            rightName = "h_scroll_right_apart_enabled";
+            break;
+        case FOREGROUND_PRESSED:
+            leftName = "h_scroll_left_apart_pressed";
+            rightName = "h_scroll_right_apart_pressed";
+        }
+        rightImage = new ImageIcon(ScrollBarButtonPainter.class.getResource(SeaGlassLookAndFeel.getImageDirectory() + "/" + rightName
+                + ".png"));
+        return leftName;
+    }
+
+    @Override
+    protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
+        if (c != null && c.getParent() != null) {
+            SeaGlassScrollBarUI ui = (SeaGlassScrollBarUI) ((JScrollBar) c.getParent()).getUI();
+            if (ui.getIncreaseButton() == c) {
+                rightImage.paintIcon(c, g, 0, 0);
+                return;
+            }
+        }
+        image.paintIcon(c, g, 0, 0);
+    }
+}
---
+++ seaglass/tags/SeaGlass_Rel_0_1_1/seaglass/src/main/java/com/seaglasslookandfeel/painter/TableHeaderPainter.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2009 Kathryn Huxtable and Kenneth Orr.
+ *
+ * This file is part of the SeaGlass Pluggable Look and Feel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id$
+ */
+package com.seaglasslookandfeel.painter;
+
+import java.awt.Dimension;
+import java.awt.Insets;
+
+import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
+
+/**
+ * TableHeaderPainter implementation.
+ */
+public final class TableHeaderPainter extends AbstractImagePainter<TableHeaderPainter.Which> {
+    public static enum Which {
+        ASCENDINGSORTICON_ENABLED, DESCENDINGSORTICON_ENABLED
+    }
+
+    public TableHeaderPainter(Which state) {
+        super(state);
+        if (state == Which.ASCENDINGSORTICON_ENABLED) {
+            setPaintContext(new PaintContext(new Insets(0, 0, 0, 2), new Dimension(8, 7), false, CacheMode.FIXED_SIZES, 1.0, 1.0));
+        } else {
+            setPaintContext(new PaintContext(new Insets(0, 0, 0, 0), new Dimension(8, 7), false, CacheMode.FIXED_SIZES, 1.0, 1.0));
+        }
+    }
+
+    protected String getImageName(Which state) {
+        switch (state) {
+        case ASCENDINGSORTICON_ENABLED:
+            return "table_header_sort_ascending_enabled";
+        case DESCENDINGSORTICON_ENABLED:
+            return "table_header_sort_descending_enabled";
+        }
+        return null;
+    }
+}
