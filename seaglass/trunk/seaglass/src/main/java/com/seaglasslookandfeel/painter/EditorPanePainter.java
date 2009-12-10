6d90991000a1a3e6ee4bb5e6f5b724ac0b14d3f0
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2009-12-10 21:46:21 +0000

    Paint using Graphics2D instead of images.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/EditorPanePainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/EditorPanePainter.java
@@ -19,39 +19,60 @@
  */
 package com.seaglasslookandfeel.painter;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Insets;
+
+import javax.swing.JComponent;
 
 import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
 
 /**
  * EditorPanePainter implementation.
  */
-public final class EditorPanePainter extends AbstractImagePainter<EditorPanePainter.Which> {
+public final class EditorPanePainter extends AbstractRegionPainter {
     public static enum Which {
         BACKGROUND_DISABLED, BACKGROUND_ENABLED, BACKGROUND_SELECTED,
     }
 
-    private static final Insets    insets    = new Insets(0, 0, 0, 0);
-    private static final Dimension dimension = new Dimension(100, 30);
-    private static final CacheMode cacheMode = CacheMode.NINE_SQUARE_SCALE;
-    private static final Double    maxH      = Double.POSITIVE_INFINITY;
-    private static final Double    maxV      = Double.POSITIVE_INFINITY;
+    private static final Insets    insets              = new Insets(0, 0, 0, 0);
+    private static final Dimension dimension           = new Dimension(100, 30);
+    private static final CacheMode cacheMode           = CacheMode.NINE_SQUARE_SCALE;
+    private static final Double    maxH                = Double.POSITIVE_INFINITY;
+    private static final Double    maxV                = Double.POSITIVE_INFINITY;
+
+    private static final Color     DISABLED_BACKGROUND = new Color(0xf8f8f8);
+    private static final Color     ENABLED_BACKGROUND  = Color.white;
+
+    private Which                  state;
+    private PaintContext           ctx;
 
     public EditorPanePainter(Which state) {
-        super(state);
-        setPaintContext(new PaintContext(insets, dimension, false, cacheMode, maxH, maxV));
+        super();
+        this.state = state;
+        ctx = new PaintContext(insets, dimension, false, cacheMode, maxH, maxV);
     }
 
-    protected String getImageName(Which state) {
+    @Override
+    protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
+        Color color = ENABLED_BACKGROUND;
         switch (state) {
         case BACKGROUND_DISABLED:
-            return "editorpane_disabled";
+            color = DISABLED_BACKGROUND;
         case BACKGROUND_ENABLED:
-            return "editorpane";
+            color = ENABLED_BACKGROUND;
         case BACKGROUND_SELECTED:
-            return "editorpane_selected";
+            color = ENABLED_BACKGROUND;
         }
-        return null;
+
+        // Paint the rectangle.
+        g.setColor(color);
+        g.fillRect(0, 0, width, height);
+    }
+
+    @Override
+    protected PaintContext getPaintContext() {
+        return ctx;
     }
 }
