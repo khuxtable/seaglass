cb2ac3b3a4783292ba3d8896d762778f2b235d4c
kathryn <kathryn@kathrynhuxtable.org>
2009-12-14 00:10:40 +0000

    Added JTextField.variant property = search to draw a rounded field. Need to add an hourglass.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TextComponentPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TextComponentPainter.java
@@ -20,13 +20,16 @@
 package com.seaglasslookandfeel.painter;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Insets;
 import java.awt.Shape;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.Path2D;
 
 import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
 
 import com.seaglasslookandfeel.effect.DropShadowEffect;
 import com.seaglasslookandfeel.effect.Effect;
@@ -47,15 +50,16 @@ public final class TextComponentPainter extends AbstractRegionPainter {
         BORDER_ENABLED,
     }
 
-    private static final Color     DISABLED_BORDER = new Color(0xdddddd);
-    private static final Color     ENABLED_BORDER  = new Color(0xbbbbbb);
+    private static final Color     DISABLED_BORDER        = new Color(0xdddddd);
+    private static final Color     ENABLED_BORDER         = new Color(0xbbbbbb);
+    private static final Color     ENABLED_TOOLBAR_BORDER = new Color(0x888888);
 
-    private static final Effect    dropShadow      = new TextFieldDropShadowEffect();
-    private static final Effect    focusGlow       = new FocusEffect();
-    private static final Dimension dimension       = new Dimension(122, 24);
-    private static final Insets    insets          = new Insets(5, 3, 3, 3);
+    private static final Effect    dropShadow             = new TextFieldDropShadowEffect();
+    private static final Effect    focusGlow              = new FocusEffect();
+    private static final Dimension dimension              = new Dimension(90, 30);
+    private static final Insets    insets                 = new Insets(5, 3, 3, 3);
 
-    Rectangle2D                    path            = new Rectangle2D.Double();
+    Path2D                         path                   = new Path2D.Double();
 
     private Which                  state;
     private PaintContext           ctx;
@@ -63,7 +67,7 @@ public final class TextComponentPainter extends AbstractRegionPainter {
     public TextComponentPainter(Which state) {
         super();
         this.state = state;
-        this.ctx = new PaintContext(insets, dimension, false, AbstractRegionPainter.PaintContext.CacheMode.NINE_SQUARE_SCALE,
+        this.ctx = new PaintContext(insets, dimension, false, AbstractRegionPainter.PaintContext.CacheMode.FIXED_SIZES,
             Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
     }
 
@@ -71,30 +75,32 @@ public final class TextComponentPainter extends AbstractRegionPainter {
      * {@inheritDoc}
      */
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
+        boolean searchType = (c instanceof JTextField && "search".equals(c.getClientProperty("JTextField.variant")));
+
         switch (state) {
         case BACKGROUND_DISABLED:
-            paintBackgroundDisabled(g, c, width, height);
+            paintBackgroundDisabled(g, c, width, height, searchType);
             break;
         case BACKGROUND_ENABLED:
-            paintBackgroundEnabled(g, c, width, height);
+            paintBackgroundEnabled(g, c, width, height, searchType);
             break;
         case BACKGROUND_SOLID_DISABLED:
-            paintBackgroundSolidDisabled(g, c, width, height);
+            paintBackgroundSolidDisabled(g, c, width, height, searchType);
             break;
         case BACKGROUND_SOLID_ENABLED:
-            paintBackgroundSolidEnabled(g, c, width, height);
+            paintBackgroundSolidEnabled(g, c, width, height, searchType);
             break;
         case BACKGROUND_SELECTED:
-            paintBackgroundEnabled(g, c, width, height);
+            paintBackgroundEnabled(g, c, width, height, searchType);
             break;
         case BORDER_DISABLED:
-            paintBorderDisabled(g, c, width, height);
+            paintBorderDisabled(g, c, width, height, searchType);
             break;
         case BORDER_ENABLED:
-            paintBorderEnabled(g, c, width, height);
+            paintBorderEnabled(g, c, width, height, searchType);
             break;
         case BORDER_FOCUSED:
-            paintBorderFocused(g, c, width, height);
+            paintBorderFocused(g, c, width, height, searchType);
             break;
         }
     }
@@ -106,56 +112,118 @@ public final class TextComponentPainter extends AbstractRegionPainter {
         return ctx;
     }
 
-    private void paintBackgroundDisabled(Graphics2D g, JComponent c, int width, int height) {
+    private void paintBackgroundDisabled(Graphics2D g, JComponent c, int width, int height, boolean searchType) {
+        setPath(3, 3, width - 6, height - 6, searchType);
         Color color = c.getBackground();
         color = new Color(color.getRed(), color.getGreen(), color.getBlue(), 0x80);
         g.setColor(color);
-        g.fillRect(3, 3, width - 7, height - 7);
+        g.fill(path);
     }
 
-    private void paintBackgroundEnabled(Graphics2D g, JComponent c, int width, int height) {
+    private void paintBackgroundEnabled(Graphics2D g, JComponent c, int width, int height, boolean searchType) {
+        setPath(3, 3, width - 6, height - 6, searchType);
         g.setColor(c.getBackground());
-        g.fillRect(3, 3, width - 7, height - 7);
+        g.fill(path);
     }
 
-    private void paintBackgroundSolidDisabled(Graphics2D g, JComponent c, int width, int height) {
+    private void paintBackgroundSolidDisabled(Graphics2D g, JComponent c, int width, int height, boolean searchType) {
+        setPath(0, 0, width, height, searchType);
         Color color = c.getBackground();
         color = new Color(color.getRed(), color.getGreen(), color.getBlue(), 0x80);
         g.setColor(color);
-        g.fillRect(0, 0, width, height);
+        g.fill(path);
     }
 
-    private void paintBackgroundSolidEnabled(Graphics2D g, JComponent c, int width, int height) {
+    private void paintBackgroundSolidEnabled(Graphics2D g, JComponent c, int width, int height, boolean searchType) {
+        setPath(0, 0, width, height, searchType);
         g.setColor(c.getBackground());
-        g.fillRect(0, 0, width, height);
+        g.fill(path);
     }
 
-    private void paintBorderDisabled(Graphics2D g, JComponent c, int width, int height) {
-        paintInternalDropShadow(g, width, height);
+    private void paintBorderDisabled(Graphics2D g, JComponent c, int width, int height, boolean searchType) {
+        paintInternalDropShadow(g, width, height, searchType);
 
         g.setColor(DISABLED_BORDER);
         g.drawRect(2, 2, width - 5, height - 5);
     }
 
-    private void paintBorderEnabled(Graphics2D g, JComponent c, int width, int height) {
-        paintInternalDropShadow(g, width, height);
+    private void paintBorderEnabled(Graphics2D g, JComponent c, int width, int height, boolean searchType) {
+        paintInternalDropShadow(g, width, height, searchType);
+        Color color = ENABLED_BORDER;
+        for (Container container = c.getParent(); container != null; container = container.getParent()) {
+            if (container instanceof JToolBar) {
+                color = ENABLED_TOOLBAR_BORDER;
+                break;
+            }
+        }
 
-        g.setColor(ENABLED_BORDER);
-        g.drawRect(2, 2, width - 5, height - 5);
+        setPath(2, 2, width - 5, height - 5, searchType);
+        g.setColor(color);
+        g.draw(path);
     }
 
-    private void paintBorderFocused(Graphics2D g, JComponent c, int width, int height) {
-        path.setRect(2, 2, width - 4, height - 4);
-        focusGlow.paint(g, path, width, height);
-        paintBorderEnabled(g, c, width, height);
+    private void paintBorderFocused(Graphics2D g, JComponent c, int width, int height, boolean searchType) {
+        paintBorderEnabled(g, c, width, height, searchType);
+        if (searchType) {
+            setPath(1, 2, width - 3, height - 4, searchType);
+        } else {
+            setPath(2, 2, width - 4, height - 4, searchType);
+        }
+        focusGlow.fill(g, path, width, height);
     }
 
-    private void paintInternalDropShadow(Graphics2D g, int width, int height) {
+    private void paintInternalDropShadow(Graphics2D g, int width, int height, boolean searchType) {
         Shape s = g.getClip();
         g.setClip(2, 2, width - 5, height - 5);
-        path.setRect(2, 2, width - 5, 2);
-        dropShadow.paint(g, path, width, height);
+        setShadowPart(2, 2, width - 6, height - 6, searchType);
+        dropShadow.draw(g, path, width, height);
         g.setClip(s);
+    }
+
+    private void setPath(int x, int y, int width, int height, boolean searchType) {
+        path.reset();
+        double x0 = x;
+        double y0 = y;
+        double x2 = x + width;
+        double y2 = y + height;
+        if (searchType) {
+            double xa = x + height / 2.0;
+            double x2a = x2 - height / 2.0;
+            double y1 = y + height / 2.0;
+            path.moveTo(xa, y);
+            path.quadTo(x0, y0, x0, y1);
+            path.quadTo(x0, y2, xa, y2);
+            path.lineTo(x2a, y2);
+            path.quadTo(x2, y2, x2, y1);
+            path.quadTo(x2, y0, x2a, y0);
+        } else {
+            path.moveTo(x0, y0);
+            path.lineTo(x0, y2);
+            path.lineTo(x2, y2);
+            path.lineTo(x2, y0);
+        }
+        path.closePath();
+    }
+
+    private void setShadowPart(int x, int y, int width, int height, boolean searchType) {
+        y++;
+        height--;
+        path.reset();
+        double x0 = x;
+        double y0 = y;
+        double x2 = x + width;
+        if (searchType) {
+            double xa = x + height / 2.0;
+            double x2a = x2 - height / 2.0;
+            double y1 = y + height / 2.0;
+            path.moveTo(x0, y1);
+            path.quadTo(x0, y, xa, y0);
+            path.lineTo(x2a, y0);
+            path.quadTo(x2, y0, x2, y1);
+        } else {
+            path.moveTo(x0, y0);
+            path.lineTo(x2, y0);
+        }
     }
 
     /**
@@ -164,10 +232,10 @@ public final class TextComponentPainter extends AbstractRegionPainter {
     public static class TextFieldDropShadowEffect extends DropShadowEffect {
 
         public TextFieldDropShadowEffect() {
-            color = new Color(211, 211, 211);
+            color = new Color(230, 230, 230);
             angle = 90;
             distance = 0;
-            size = 2;
+            size = 3;
             opacity = 0.35f;
         }
     }
