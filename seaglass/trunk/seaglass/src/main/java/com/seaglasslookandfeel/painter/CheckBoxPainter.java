01fc732f77cf33d96f0358f4b470b7b8c8e6c10c
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2009-12-14 20:15:42 +0000

    Paint check boxes with Graphics2D instead of with images.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/CheckBoxPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/CheckBoxPainter.java
@@ -19,18 +19,31 @@
  */
 package com.seaglasslookandfeel.painter;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.Paint;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.Path2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
 
+import javax.swing.JComponent;
+
+import com.seaglasslookandfeel.effect.DropShadowEffect;
+import com.seaglasslookandfeel.effect.Effect;
+import com.seaglasslookandfeel.effect.SeaGlassDropShadowEffect;
 import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
 
 /**
  * CheckBoxPainter implementation.
  */
-public final class CheckBoxPainter extends AbstractImagePainter<CheckBoxPainter.Which> {
+public final class CheckBoxPainter extends AbstractRegionPainter {
     public static enum Which {
-        BACKGROUND_DISABLED,
-        BACKGROUND_ENABLED,
         ICON_DISABLED,
         ICON_ENABLED,
         ICON_FOCUSED,
@@ -43,44 +56,285 @@ public final class CheckBoxPainter extends AbstractImagePainter<CheckBoxPainter.
         ICON_DISABLED_SELECTED,
     }
 
-    private static final Insets    insets    = new Insets(5, 5, 5, 5);
-    private static final Dimension dimension = new Dimension(16, 9);
-    private static final CacheMode cacheMode = CacheMode.FIXED_SIZES;
-    private static final Double    maxH      = 1.0;
-    private static final Double    maxV      = 1.0;
+    private static final Insets    insets                   = new Insets(0, 0, 0, 0);
+    private static final Dimension dimension                = new Dimension(18, 19);
+    private static final CacheMode cacheMode                = CacheMode.FIXED_SIZES;
+    private static final Double    maxH                     = 1.0;
+    private static final Double    maxV                     = 1.0;
+
+    private Which                  state;
+    private PaintContext           ctx;
+    private boolean                focused;
+    private boolean                selected;
+
+    private final Color            colorShadow              = new Color(0x000000);
+    final Color                    colorFocus               = new Color(0x79a0cf);
+    private Effect                 dropShadow               = new SeaGlassDropShadowEffect();
+
+    private RoundRectangle2D       rect                     = new RoundRectangle2D.Double();
+    private Path2D                 path                     = new Path2D.Double();
+
+    private Color                  bullet1                  = new Color(0x333333);
+    private Color                  bullet2                  = new Color(0x000000);
+
+    private Color                  bulletDisabled1          = new Color(0x80333333, true);
+    private Color                  bulletDisabled2          = new Color(0x80000000, true);
+
+    private Color                  enabledInternal1         = new Color(0xfbfdfe);
+    private Color                  enabledInternal2         = new Color(0xd6eaf9);
+    private Color                  enabledInternal3         = new Color(0xd2e8f8);
+    private Color                  enabledInternal4         = new Color(0xf5fafd);
+    private Color                  enabledBorder1           = new Color(0x88ade0);
+    private Color                  enabledBorder2           = new Color(0x5785bf);
+
+    private Color                  pressedInternal1         = new Color(0xacbdd0);
+    private Color                  pressedInternal2         = new Color(0x688db3);
+    private Color                  pressedInternal3         = new Color(0x6d93ba);
+    private Color                  pressedInternal4         = new Color(0xa4cbe4);
+    private Color                  pressedBorder1           = new Color(0x4f7bbf);
+    private Color                  pressedBorder2           = new Color(0x3f76bf);
+
+    private Color                  selectedInternal1        = new Color(0xbccedf);
+    private Color                  selectedInternal2        = new Color(0x7fa7cd);
+    private Color                  selectedInternal3        = new Color(0x82b0d6);
+    private Color                  selectedInternal4        = new Color(0xb0daf6);
+    private Color                  selectedBorder1          = new Color(0x4f7bbf);
+    private Color                  selectedBorder2          = new Color(0x3f76bf);
+
+    private Color                  pressedSelectedInternal1 = new Color(0xacbdd0);
+    private Color                  pressedSelectedInternal2 = new Color(0x688db3);
+    private Color                  pressedSelectedInternal3 = new Color(0x6d93ba);
+    private Color                  pressedSelectedInternal4 = new Color(0xa4cbe4);
+    private Color                  pressedSelectedBorder1   = new Color(0x4f7bbf);
+    private Color                  pressedSelectedBorder2   = new Color(0x3f76bf);
+
+    private Color                  disabledInternalA1       = new Color(0x60f4f8fb, true);
+    private Color                  disabledInternalA2       = new Color(0x00ffffff, true);
+    private Color                  disabledInternalB1       = new Color(0x00a8d9fc, true);
+    private Color                  disabledInternalB2       = new Color(0x00cfeafd, true);
+    private Color                  disabledInternalB3       = new Color(0x80f7fcff, true);
+    private Color                  disabledInternalC        = new Color(0x80eeeeee, true);
+    private Color                  disabledBorder1          = new Color(0x808aafe0, true);
+    private Color                  disabledBorder2          = new Color(0x805785bf, true);
 
     public CheckBoxPainter(Which state) {
-        super(state);
-        PaintContext ctx = new PaintContext(insets, dimension, false, cacheMode, maxH, maxV);
-        setPaintContext(ctx);
+        super();
+        this.state = state;
+        ctx = new PaintContext(insets, dimension, false, cacheMode, maxH, maxV);
+
+        focused = false;
+        selected = false;
+        if (state == Which.ICON_FOCUSED || state == Which.ICON_PRESSED_FOCUSED || state == Which.ICON_SELECTED_FOCUSED
+                || state == Which.ICON_PRESSED_SELECTED_FOCUSED) {
+            focused = true;
+        }
+        if (state == Which.ICON_SELECTED || state == Which.ICON_PRESSED_SELECTED || state == Which.ICON_DISABLED_SELECTED
+                || state == Which.ICON_SELECTED_FOCUSED || state == Which.ICON_PRESSED_SELECTED_FOCUSED) {
+            selected = true;
+        }
     }
 
     @Override
-    protected String getImageName(Which state) {
-        // generate this entire method. Each state/bg/fg/border combo that has
-        // been painted gets its own KEY and paint method.
+    protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
         switch (state) {
         case ICON_DISABLED:
-            return "check_box_disabled";
+            paintDisabled(g, c, width, height);
+            break;
         case ICON_ENABLED:
-            return "check_box_enabled";
         case ICON_FOCUSED:
-            return "check_box_enabled";
+            paintEnabled(g, c, width, height);
+            break;
         case ICON_PRESSED:
-            return "check_box_pressed";
         case ICON_PRESSED_FOCUSED:
-            return "check_box_pressed";
+            paintPressed(g, c, width, height);
+            break;
         case ICON_SELECTED:
-            return "check_box_selected";
         case ICON_SELECTED_FOCUSED:
-            return "check_box_selected";
+            paintSelected(g, c, width, height);
+            break;
         case ICON_PRESSED_SELECTED:
-            return "check_box_selected_pressed";
         case ICON_PRESSED_SELECTED_FOCUSED:
-            return "check_box_selected_pressed";
+            paintPressedSelected(g, c, width, height);
+            break;
         case ICON_DISABLED_SELECTED:
-            return "check_box_disabled_selected";
+            paintDisabledSelected(g, c, width, height);
+            break;
         }
-        return null;
+
+        if (focused) {
+            Shape s = setFocus(width, height);
+            g.setColor(colorFocus);
+            g.setStroke(new BasicStroke(1.5f));
+            g.draw(s);
+        }
+        if (selected) {
+            if (state == Which.ICON_DISABLED_SELECTED) {
+                paintCheckMark(g, width, height, bulletDisabled1, bulletDisabled2, false);
+            } else {
+                paintCheckMark(g, width, height, bullet1, bullet2, false);
+            }
+        }
+    }
+
+    @Override
+    protected PaintContext getPaintContext() {
+        return ctx;
+    }
+
+    private void paintDisabled(Graphics2D g, JComponent c, int width, int height) {
+        paintFourLayeredButton(g, width, height, disabledBorder1, disabledBorder2, disabledInternalA1, disabledInternalA2,
+            disabledInternalB1, disabledInternalB2, disabledInternalB3, disabledInternalC);
+    }
+
+    private void paintEnabled(Graphics2D g, JComponent c, int width, int height) {
+        paintTwoLayeredButton(g, width, height, enabledBorder1, enabledBorder2, enabledInternal1, enabledInternal2, enabledInternal3,
+            enabledInternal4);
+    }
+
+    private void paintPressed(Graphics2D g, JComponent c, int width, int height) {
+        paintTwoLayeredButton(g, width, height, pressedBorder1, pressedBorder2, pressedInternal1, pressedInternal2, pressedInternal3,
+            pressedInternal4);
+    }
+
+    private void paintSelected(Graphics2D g, JComponent c, int width, int height) {
+        paintTwoLayeredButton(g, width, height, selectedBorder1, selectedBorder2, selectedInternal1, selectedInternal2, selectedInternal3,
+            selectedInternal4);
+    }
+
+    private void paintPressedSelected(Graphics2D g, JComponent c, int width, int height) {
+        paintTwoLayeredButton(g, width, height, pressedSelectedBorder1, pressedSelectedBorder2, pressedSelectedInternal1,
+            pressedSelectedInternal2, pressedSelectedInternal3, pressedSelectedInternal4);
+    }
+
+    private void paintDisabledSelected(Graphics2D g, JComponent c, int width, int height) {
+        paintFourLayeredButton(g, width, height, disabledBorder1, disabledBorder2, disabledInternalA1, disabledInternalA2,
+            disabledInternalB1, disabledInternalB2, disabledInternalB3, disabledInternalC);
+    }
+
+    private void paintTwoLayeredButton(Graphics2D g, int width, int height, Color border1, Color border2, Color internal1, Color internal2,
+        Color internal3, Color internal4) {
+        Shape s = setBorder(width, height);
+        g.drawImage(createDropShadowImage(s), 0, 0, null);
+        Paint p = setGradient2(s, border1, border2);
+        g.setPaint(p);
+        g.fill(s);
+        s = setInternal(width, height);
+        p = setGradient4(s, internal1, internal2, internal3, internal4);
+        g.setPaint(p);
+        g.fill(s);
+    }
+
+    private void paintFourLayeredButton(Graphics2D g, int width, int height, Color border1, Color border2, Color internalA1,
+        Color internalA2, Color internalB1, Color internalB2, Color internalB3, Color internalC) {
+        Shape s = setBorder(width, height);
+        g.drawImage(createDropShadowImage(s), 0, 0, null);
+        Paint p = setGradient2(s, border1, border2);
+        g.setPaint(p);
+        g.fill(s);
+        s = setInternal(width, height);
+        g.setColor(internalC);
+        g.fill(s);
+        p = setGradient3(s, internalB1, internalB2, internalB3);
+        g.setPaint(p);
+        g.fill(s);
+        p = setGradient2(s, internalA1, internalA2);
+        g.setPaint(p);
+        g.fill(s);
+    }
+
+    private void paintCheckMark(Graphics2D g, int width, int height, Color color1, Color color2, boolean shadow) {
+        Shape s = setCheckMark(width, height);
+        Paint p = setGradientCheckMark(s, color1, color2);
+        g.setPaint(p);
+        g.fill(s);
+    }
+
+    private Shape setFocus(int width, int height) {
+        return setRect(width - 1.0, width, height, 7);
+    }
+
+    private Shape setBorder(int width, int height) {
+        return setRect(width - 2.0, width, height, 6);
+    }
+
+    private Shape setInternal(int width, int height) {
+        return setRect(width - 4.0, width, height, 4);
+    }
+
+    /**
+     * Create a drop shadow image.
+     * 
+     * @param s
+     *            the shape to use as the shade.
+     */
+    BufferedImage createDropShadowImage(Shape s) {
+        BufferedImage bimage = DropShadowEffect.createBufferedImage(dimension.width, dimension.height, true);
+        Graphics2D gbi = bimage.createGraphics();
+        gbi.setColor(colorShadow);
+        gbi.fill(s);
+        return dropShadow.applyEffect(bimage, null, dimension.width, dimension.height);
+    }
+
+    private Shape setRect(Double diameter, int width, int height, int arc) {
+        Double pos = (width - diameter) / 2.0;
+        rect.setRoundRect(pos, pos + 2.0 / 19.0 * height, diameter, diameter, arc, arc);
+        return rect;
+    }
+
+    private Shape setCheckMark(int width, int height) {
+        double widthMult = width / 16.0;
+        double heightMult = height / 19.0;
+        path.reset();
+        path.moveTo(5.0 * widthMult, 9.0 * heightMult);
+        path.lineTo(7.0 * widthMult, 9.0 * heightMult);
+        path.lineTo(9.75 * widthMult, 12.0 * heightMult);
+        path.lineTo(14.0 * widthMult, 2.0 * heightMult);
+        path.lineTo(16.0 * widthMult, 2.0 * heightMult);
+        path.lineTo(10.0 * widthMult, 14.0 * heightMult);
+        path.closePath();
+        return path;
+    }
+
+    private Paint setGradient2(Shape s, Color color1, Color color2) {
+        Rectangle2D bounds = s.getBounds2D();
+        float x = (float) bounds.getX();
+        float y = (float) bounds.getY();
+        float w = (float) bounds.getWidth();
+        float h = (float) bounds.getHeight();
+        return decodeGradient((0.5f * w) + x, y, (0.5f * w) + x, h + y, new float[] { 0f, 1f }, new Color[] { color1, color2 });
+    }
+
+    private Paint setGradient3(Shape s, Color color1, Color color2, Color color3) {
+        Rectangle2D bounds = s.getBounds2D();
+        float x = (float) bounds.getX();
+        float y = (float) bounds.getY();
+        float w = (float) bounds.getWidth();
+        float h = (float) bounds.getHeight();
+        return decodeGradient((0.5f * w) + x, y, (0.5f * w) + x, h + y, new float[] { 0f, 0.5f, 1f },
+            new Color[] { color1, color2, color3 });
+    }
+
+    private Paint setGradient4(Shape s, Color color1, Color color2, Color color3, Color color4) {
+        Rectangle2D bounds = s.getBounds2D();
+        float x = (float) bounds.getX();
+        float y = (float) bounds.getY();
+        float w = (float) bounds.getWidth();
+        float h = (float) bounds.getHeight();
+        return decodeGradient((0.5f * w) + x, y, (0.5f * w) + x, h + y, new float[] { 0f, 0.45f, 0.6f, 1f }, new Color[] {
+            color1,
+            color2,
+            color3,
+            color4 });
+    }
+
+    private Paint setGradientCheckMark(Shape s, Color color1, Color color2) {
+        Rectangle2D bounds = s.getBounds2D();
+        float x = (float) bounds.getX();
+        float y = (float) bounds.getY();
+        float w = (float) bounds.getWidth();
+        float h = (float) bounds.getHeight();
+        return decodeGradient(x + w, y, (0.3f * w) + x, h + y, new float[] { 0f, 1f }, new Color[] { color1, color2 });
     }
 }
