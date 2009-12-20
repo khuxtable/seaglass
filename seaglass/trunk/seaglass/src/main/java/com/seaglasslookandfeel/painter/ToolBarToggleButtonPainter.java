ef49f81fd484261e564f3eac1f2c19fa02f509e9
kathryn <kathryn@kathrynhuxtable.org>
2009-12-20 20:50:17 +0000

    Paint toggle button using Graphics2D and use the current Mac look for selected button.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/ToolBarToggleButtonPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/ToolBarToggleButtonPainter.java
@@ -19,15 +19,23 @@
  */
 package com.seaglasslookandfeel.painter;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.JComponent;
 
 import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
 
 /**
  * ToolBarToggleButtonPainter implementation.
  */
-public final class ToolBarToggleButtonPainter extends AbstractImagePainter<ToolBarToggleButtonPainter.Which> {
+public final class ToolBarToggleButtonPainter extends AbstractRegionPainter {
     public static enum Which {
         BACKGROUND_ENABLED,
         BACKGROUND_FOCUSED,
@@ -40,37 +48,83 @@ public final class ToolBarToggleButtonPainter extends AbstractImagePainter<ToolB
         BACKGROUND_DISABLED_SELECTED,
     }
 
-    private static final Insets    insets    = new Insets(5, 5, 5, 5);
-    private static final Dimension dimension = new Dimension(104, 34);
-    private static final CacheMode cacheMode = CacheMode.NINE_SQUARE_SCALE;
-    private static final Double    maxH      = 2.0;
-    private static final Double    maxV      = Double.POSITIVE_INFINITY;
+    private static final Color     END_INNER_COLOR      = new Color(0x00000000, true);
+    private static final Color     MID_INNER_COLOR      = new Color(0x20000000, true);
+
+    private static final Color     END_INNER_EDGE_COLOR = new Color(0x00000000, true);
+    private static final Color     MID_INNER_EDGE_COLOR = new Color(0x20000000, true);
+
+    private static final Color     END_OUTER_EDGE_COLOR = new Color(0x10000000, true);
+    private static final Color     MID_OUTER_EDGE_COLOR = new Color(0x40000000, true);
+
+    private static final Insets    insets               = new Insets(5, 5, 5, 5);
+    private static final Dimension dimension            = new Dimension(104, 34);
+    private static final CacheMode cacheMode            = CacheMode.FIXED_SIZES;
+    private static final Double    maxH                 = Double.POSITIVE_INFINITY;
+    private static final Double    maxV                 = Double.POSITIVE_INFINITY;
+
+    private Rectangle              rect                 = new Rectangle();
+
+    private Which                  state;
+    private PaintContext           ctx;
 
     public ToolBarToggleButtonPainter(Which state) {
-        super(state);
-        PaintContext ctx = new PaintContext(insets, dimension, false, cacheMode, maxH, maxV);
-        setPaintContext(ctx);
+        super();
+        this.state = state;
+        ctx = new PaintContext(insets, dimension, false, cacheMode, maxH, maxV);
     }
 
-    protected String getImageName(Which state) {
+    @Override
+    protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
         switch (state) {
-        case BACKGROUND_FOCUSED:
-            return "toolbar_button_focused";
-        case BACKGROUND_PRESSED:
-            return "toolbar_button_pressed";
-        case BACKGROUND_PRESSED_FOCUSED:
-            return "toolbar_button_focused_pressed";
         case BACKGROUND_SELECTED:
-            return "toolbar_button_selected";
         case BACKGROUND_SELECTED_FOCUSED:
-            return "toolbar_button_selected_focused";
         case BACKGROUND_PRESSED_SELECTED:
-            return "toolbar_button_selected";
         case BACKGROUND_PRESSED_SELECTED_FOCUSED:
-            return "toolbar_button_selected_focused";
         case BACKGROUND_DISABLED_SELECTED:
-            return "toolbar_button_selected";
+            paintSelectedBackground(g, width, height);
         }
-        return null;
+    }
+
+    @Override
+    protected PaintContext getPaintContext() {
+        return ctx;
+    }
+
+    private void paintSelectedBackground(Graphics2D g, int width, int height) {
+        paintRectangle(g, 0, height, END_INNER_COLOR, MID_INNER_COLOR);
+
+        paintRectangle(g, 1, height, END_INNER_EDGE_COLOR, MID_INNER_EDGE_COLOR);
+        paintRectangle(g, width - 2, height, END_INNER_EDGE_COLOR, MID_INNER_EDGE_COLOR);
+
+        paintRectangle(g, 0, height, END_OUTER_EDGE_COLOR, MID_OUTER_EDGE_COLOR);
+        paintRectangle(g, width - 1, height, END_OUTER_EDGE_COLOR, MID_OUTER_EDGE_COLOR);
+    }
+
+    private void paintRectangle(Graphics2D g, int x, int height, Color endColor, Color midColor) {
+        rect.setBounds(x, 0, 1, height);
+        g.setPaint(decodeGradient(rect, endColor, midColor));
+        g.fill(rect);
+    }
+
+    /**
+     * Create the gradient for the background of the button. This creates the
+     * border.
+     * 
+     * @param s
+     * @param color1
+     * @param color2
+     * @return
+     */
+    Paint decodeGradient(Shape s, Color endColor, Color middleColor) {
+        Rectangle2D bounds = s.getBounds2D();
+        float x = (float) bounds.getX();
+        float y = (float) bounds.getY();
+        float w = (float) bounds.getWidth();
+        float h = (float) bounds.getHeight();
+        return decodeGradient((0.5f * w) + x, y, (0.5f * w) + x, h + y, new float[] { 0f, 0.5f, 1f }, new Color[] {
+            endColor,
+            middleColor,
+            endColor });
     }
 }
