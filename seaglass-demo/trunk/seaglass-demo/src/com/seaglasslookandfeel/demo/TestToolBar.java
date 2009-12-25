5d7d75a93ed6a71c424f461016221b462ee12435
kathryn <kathryn@kathrynhuxtable.org>
2009-12-25 00:55:34 +0000

    Added a menu bar.

--- seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestToolBar.java
+++ seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestToolBar.java
@@ -6,6 +6,8 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
@@ -26,6 +28,14 @@ public class TestToolBar {
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                
+                JMenuBar mb = new JMenuBar();
+                JMenu m1 = new JMenu("File");
+                JMenu m2 = new JMenu("Edit");
+                JMenu m3 = new JMenu("Help");
+                mb.add(m1);
+                mb.add(m2);
+                mb.add(m3);
 
                 JButton button1 = new JButton("Test 1");
                 JToggleButton button2 = new JToggleButton("Test 2");
@@ -42,6 +52,7 @@ public class TestToolBar {
                 toolbar.add(foo);
 
                 JFrame frame = new JFrame();
+                frame.setJMenuBar(mb);
                 frame.getRootPane().putClientProperty("apple.awt.brushMetalLook", Boolean.TRUE);
                 
                 JPanel panel = new JPanel();
