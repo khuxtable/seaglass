a85dc543bf8b83fb2e35316a6bcd9395fd739824
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2010-01-01 18:44:18 +0000

    Draw the track with Graphics2D instead of using an image.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/ScrollBarTrackPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/ScrollBarTrackPainter.java
@@ -19,37 +19,64 @@
  */
 package com.seaglasslookandfeel.painter;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Insets;
+
+import javax.swing.JComponent;
 
 import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
 
 /**
  * ScrollBarTrackPainter implementation.
  */
-public final class ScrollBarTrackPainter extends AbstractImagePainter<ScrollBarTrackPainter.Which> {
+public final class ScrollBarTrackPainter extends AbstractRegionPainter {
     public static enum Which {
         BACKGROUND_DISABLED, BACKGROUND_ENABLED
     }
 
+    private static final Color[]   colors    = {
+        new Color(0xbdbdbd),
+        new Color(0xcccccc),
+        new Color(0xd8d8d8),
+        new Color(0xe3e3e3),
+        new Color(0xeaeaea),
+        new Color(0xeaeaea),
+        new Color(0xf0f0f0),
+        new Color(0xf5f5f5),
+        new Color(0xf6f6f6),
+        new Color(0xf7f7f7),
+        new Color(0xf8f8f8),
+        new Color(0xf9f9f9),
+        new Color(0xf7f7f7),
+        new Color(0xf3f3f3),
+        new Color(0xededed),
+        new Color(0xe4e4e4),                };
+
     private static final Insets    insets    = new Insets(0, 0, 0, 0);
     private static final Dimension dimension = new Dimension(19, 15);
-    private static final CacheMode cacheMode = CacheMode.NINE_SQUARE_SCALE;
+    private static final CacheMode cacheMode = CacheMode.FIXED_SIZES;
     private static final Double    maxH      = Double.POSITIVE_INFINITY;
-    private static final Double    maxV      = 2.0;
+    private static final Double    maxV      = 1.0;
+
+    private PaintContext           ctx;
 
     public ScrollBarTrackPainter(Which state) {
-        super(state);
-        setPaintContext(new PaintContext(insets, dimension, false, cacheMode, maxH, maxV));
+        super();
+        ctx = new PaintContext(insets, dimension, false, cacheMode, maxH, maxV);
     }
 
-    protected String getImageName(Which state) {
-        switch (state) {
-        case BACKGROUND_DISABLED:
-            return "h_scroll_track_disabled";
-        case BACKGROUND_ENABLED:
-            return "h_scroll_track_enabled";
+    @Override
+    protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
+        for (int i = 0; i < 15; i++) {
+            g.setColor(colors[i]);
+            g.drawLine(0, i, width - 1, i);
         }
-        return null;
+    }
+
+    @Override
+    protected PaintContext getPaintContext() {
+        return ctx;
     }
 }
