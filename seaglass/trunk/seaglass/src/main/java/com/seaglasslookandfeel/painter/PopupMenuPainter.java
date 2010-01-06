5ebad3e7c2a8f3fca6c4d94fd9d84d52cbd2b79e
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2010-01-06 19:24:29 +0000

    Update to Sea Glass specifications, that is, make it a round rectangle and make the colors correct.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/PopupMenuPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/PopupMenuPainter.java
@@ -23,9 +23,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Insets;
-import java.awt.Paint;
 import java.awt.Shape;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.JComponent;
 
@@ -44,18 +43,19 @@ public final class PopupMenuPainter extends AbstractRegionPainter {
     private Which                  state;
     private PaintContext           ctx;
 
-    private Rectangle2D            rect      = new Rectangle2D.Float(0, 0, 0, 0);
+    private RoundRectangle2D       rect                  = new RoundRectangle2D.Double();
 
-    private Color                  color1    = decodeColor("nimbusBlueGrey", -0.6111111f, -0.110526316f, -0.39607844f, 0);
-    private Color                  color2    = decodeColor("nimbusBase", 0.0f, -0.6357143f, 0.45098037f, 0);
-    private Color                  color3    = decodeColor("nimbusBase", 0.021348298f, -0.6150531f, 0.39999998f, 0);
+    private Color                  disabledBorderColor   = new Color(0x80dddddd, true);
+    private Color                  disabledInteriorColor = new Color(0x80ffffff, true);
 
-    // FIXME These are not assigned properly.
-    private static final Insets    insets    = new Insets(7, 7, 7, 7);
-    private static final Dimension dimension = new Dimension(86, 28);
-    private static final CacheMode cacheMode = CacheMode.NINE_SQUARE_SCALE;
-    private static final Double    maxH      = Double.POSITIVE_INFINITY;
-    private static final Double    maxV      = Double.POSITIVE_INFINITY;
+    private Color                  enabledBorderColor    = new Color(0xdddddd);
+    private Color                  enabledInteriorColor  = new Color(0xffffff);
+
+    private static final Insets    insets                = new Insets(11, 2, 11, 2);
+    private static final Dimension dimension             = new Dimension(220, 313);
+    private static final CacheMode cacheMode             = CacheMode.NO_CACHING;
+    private static final Double    maxH                  = 1.0;
+    private static final Double    maxV                  = 1.0;
 
     public PopupMenuPainter(Which state) {
         super();
@@ -66,10 +66,10 @@ public final class PopupMenuPainter extends AbstractRegionPainter {
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
         switch (state) {
         case BACKGROUND_DISABLED:
-            paintBackgroundDisabled(g);
+            paintBackgroundDisabled(g, width, height);
             break;
         case BACKGROUND_ENABLED:
-            paintBackgroundEnabled(g);
+            paintBackgroundEnabled(g, width, height);
             break;
         }
     }
@@ -78,78 +78,33 @@ public final class PopupMenuPainter extends AbstractRegionPainter {
         return ctx;
     }
 
-    private void paintBackgroundDisabled(Graphics2D g) {
-        rect = decodeRect1();
-        g.setPaint(color1);
-        g.fill(rect);
-        rect = decodeRect2();
-        g.setPaint(decodeGradient1(rect));
-        g.fill(rect);
+    private void paintBackgroundDisabled(Graphics2D g, int width, int height) {
+        Shape s = decodeBorder(width, height);
+        g.setPaint(disabledBorderColor);
+        g.fill(s);
+        s = decodeInterior(width, height);
+        g.setPaint(disabledInteriorColor);
+        g.fill(s);
 
     }
 
-    private void paintBackgroundEnabled(Graphics2D g) {
-        rect = decodeRect3();
-        g.setPaint(color1);
-        g.fill(rect);
-        rect = decodeRect4();
-        g.setPaint(decodeGradient1(rect));
-        g.fill(rect);
+    private void paintBackgroundEnabled(Graphics2D g, int width, int height) {
+        Shape s = decodeBorder(width, height);
+        g.setPaint(enabledBorderColor);
+        g.fill(s);
+        s = decodeInterior(width, height);
+        g.setPaint(enabledInteriorColor);
+        g.fill(s);
 
     }
 
-    private Rectangle2D decodeRect1() {
-        rect.setRect(decodeX(1.0f), // x
-            decodeY(0.0f), // y
-            decodeX(2.0f) - decodeX(1.0f), // width
-            decodeY(3.0f) - decodeY(0.0f)); // height
+    private Shape decodeBorder(int width, int height) {
+        rect.setRoundRect(0, 0, width, height, 6, 6);
         return rect;
     }
 
-    private Rectangle2D decodeRect2() {
-        rect.setRect(decodeX(1.0045455f), // x
-            decodeY(0.11111111f), // y
-            decodeX(1.9954545f) - decodeX(1.0045455f), // width
-            decodeY(2.909091f) - decodeY(0.11111111f)); // height
+    private Shape decodeInterior(int width, int height) {
+        rect.setRoundRect(1, 1, width - 2, height - 2, 5, 5);
         return rect;
-    }
-
-    private Rectangle2D decodeRect3() {
-        rect.setRect(decodeX(0.0f), // x
-            decodeY(0.0f), // y
-            decodeX(3.0f) - decodeX(0.0f), // width
-            decodeY(3.0f) - decodeY(0.0f)); // height
-        return rect;
-    }
-
-    private Rectangle2D decodeRect4() {
-        rect.setRect(decodeX(0.5f), // x
-            decodeY(0.09090909f), // y
-            decodeX(2.5f) - decodeX(0.5f), // width
-            decodeY(2.909091f) - decodeY(0.09090909f)); // height
-        return rect;
-    }
-
-    private Paint decodeGradient1(Shape s) {
-        Rectangle2D bounds = s.getBounds2D();
-        float x = (float) bounds.getX();
-        float y = (float) bounds.getY();
-        float w = (float) bounds.getWidth();
-        float h = (float) bounds.getHeight();
-        return decodeGradient((0.5f * w) + x, (0.0f * h) + y, (0.5f * w) + x, (1.0f * h) + y, new float[] {
-            0.0f,
-            0.0030f,
-            0.02f,
-            0.5f,
-            0.98f,
-            0.996f,
-            1.0f }, new Color[] {
-            color2,
-            decodeColor(color2, color3, 0.5f),
-            color3,
-            decodeColor(color3, color3, 0.5f),
-            color3,
-            decodeColor(color3, color2, 0.5f),
-            color2 });
     }
 }
