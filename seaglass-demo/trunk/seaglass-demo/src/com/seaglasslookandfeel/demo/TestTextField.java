0d0379c46e333d589acf869d8796ba24a460fb60
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2010-01-18 15:19:35 +0000

    Pretty much do everything I can with search fields.

--- seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestTextField.java
+++ seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestTextField.java
@@ -1,11 +1,16 @@
 package com.seaglasslookandfeel.demo;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.PopupMenu;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -21,11 +26,26 @@ public class TestTextField {
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                JPopupMenu popupMenu = new JPopupMenu();
+                popupMenu.add("First item");
+                popupMenu.add("Second item");
+                popupMenu.add("Third item");
+
                 JTextField tf1 = new JTextField("Here is some text");
                 tf1.setPreferredSize(new Dimension(180, 25));
                 tf1.putClientProperty("JTextField.variant", "search");
                 tf1.putClientProperty("JTextField.Search.PlaceholderText", "Search");
-                tf1.putClientProperty("JTextField.Search.FindPopup", new PopupMenu());
+                tf1.putClientProperty("JTextField.Search.FindPopup", popupMenu);
+                tf1.putClientProperty("JTextField.Search.CancelAction", new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("cancel source = " + e.getSource().getClass().getName());
+                    }
+                });
+                tf1.putClientProperty("JTextField.Search.FindAction", new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("find source = " + e.getSource().getClass().getName());
+                    }
+                });
 
                 JTextField tf2 = new JTextField("Here is some more text");
                 tf2.setPreferredSize(new Dimension(180, 25));
@@ -42,5 +62,25 @@ public class TestTextField {
                 frame.setVisible(true);
             }
         });
+    }
+    
+    public static class MyLayout implements LayoutManager {
+
+        public void addLayoutComponent(String name, Component comp) {
+        }
+
+        public void layoutContainer(Container parent) {
+        }
+
+        public Dimension minimumLayoutSize(Container parent) {
+            return null;
+        }
+
+        public Dimension preferredLayoutSize(Container parent) {
+            return null;
+        }
+
+        public void removeLayoutComponent(Component comp) {
+        }
     }
 }
