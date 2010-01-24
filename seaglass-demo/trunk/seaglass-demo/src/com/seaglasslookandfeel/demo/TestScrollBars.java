8ffd415633d26f28f397c1cc64b6983e04a1ec84
kathryn <kathryn@kathrynhuxtable.org>
2010-01-24 14:02:42 +0000

    Use a scroll pane for the scroll bar test.

--- seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestScrollBars.java
+++ seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestScrollBars.java
@@ -1,34 +1,37 @@
 package com.seaglasslookandfeel.demo;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 public class TestScrollBars {
 
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (true) {
+            try {
+                UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
 
-                JScrollBar scrollBar = new JScrollBar(JScrollBar.VERTICAL);
-                scrollBar.setPreferredSize(new Dimension(15, 300));
-                
                 JPanel panel = new JPanel();
-                panel.add(scrollBar, BorderLayout.CENTER);
+                panel.setPreferredSize(new Dimension(500, 500));
+                panel.setBackground(Color.WHITE);
+
+                JScrollPane scrollPane = new JScrollPane(panel);
 
                 JFrame frame = new JFrame();
-                frame.add(panel, BorderLayout.CENTER);
+                frame.add(scrollPane, BorderLayout.CENTER);
                 frame.setSize(275, 125);
                 frame.setLocationRelativeTo(null);
                 frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
