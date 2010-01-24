54d3486cd0ce2b822f6a03075af4a545e546f5f0
kathryn <kathryn@kathrynhuxtable.org>
2010-01-24 23:35:13 +0000

    It turns out the track can't paint the top/left cap because then it's the wrong size for the thumb tracking. So remove the painter states for the track with buttons together.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/ScrollBarTrackPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/ScrollBarTrackPainter.java
@@ -24,13 +24,10 @@ import java.awt.Graphics2D;
 import java.awt.Paint;
 import java.awt.Rectangle;
 import java.awt.Shape;
-import java.awt.geom.Path2D;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.JComponent;
 
-import com.seaglasslookandfeel.effect.DropShadowEffect;
-import com.seaglasslookandfeel.effect.Effect;
 import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
 
 /**
@@ -38,7 +35,7 @@ import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheM
  */
 public final class ScrollBarTrackPainter extends AbstractRegionPainter {
     public static enum Which {
-        BACKGROUND_DISABLED, BACKGROUND_ENABLED, BACKGROUND_DISABLED_TOGETHER, BACKGROUND_ENABLED_TOGETHER,
+        BACKGROUND_DISABLED, BACKGROUND_ENABLED,
     }
 
     private static final Color             trackBackground1 = new Color(0xeeeeee);
@@ -49,30 +46,18 @@ public final class ScrollBarTrackPainter extends AbstractRegionPainter {
     private static final Color             trackInner3      = new Color(0x00000000, true);
     private static final Color             trackInner4      = new Color(0x12000000, true);
 
-    private static final ButtonStateColors enabledDecrease  = new ButtonStateColors(new Color(0xffffff), new Color(0xbbbbbb), new Color(
-                                                                0xbdbdbd));
-
-    private final Color                    colorShadow      = new Color(0x000000);
-    private Effect                         dropShadow       = new ScrollButtonDropShadowEffect();
-
-    private Path2D                         path             = new Path2D.Double();
     private Rectangle2D                    rect             = new Rectangle2D.Double();
 
-    private Which                          state;
     private PaintContext                   ctx;
 
     public ScrollBarTrackPainter(Which state) {
         super();
-        this.state = state;
         this.ctx = new PaintContext(CacheMode.FIXED_SIZES);
     }
 
     @Override
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
         paintBackgroundTrack(g, width, height);
-        if (state == Which.BACKGROUND_DISABLED_TOGETHER || state == Which.BACKGROUND_ENABLED_TOGETHER) {
-            paintLeftCap(g, width, height);
-        }
     }
 
     @Override
@@ -86,15 +71,6 @@ public final class ScrollBarTrackPainter extends AbstractRegionPainter {
         g.fill(s);
         g.setPaint(decodeGradientTrackInnerShadow(s));
         g.fill(s);
-    }
-
-    private void paintLeftCap(Graphics2D g, int width, int height) {
-        Shape s = decodeTopCapPath(width, height);
-        dropShadow.fill(g, s, colorShadow);
-        g.setPaint(decodeTopCapGradient(s, enabledDecrease.top, enabledDecrease.bottom));
-        g.fill(s);
-        g.setColor(enabledDecrease.line);
-        g.drawLine(0, 0, width - 1, 0);
     }
 
     private Paint decodeGradientTrack(Shape s) {
@@ -114,54 +90,8 @@ public final class ScrollBarTrackPainter extends AbstractRegionPainter {
             new Color[] { trackInner1, trackInner2, trackInner3, trackInner3, trackInner4 });
     }
 
-    private Paint decodeTopCapGradient(Shape s, Color top, Color bottom) {
-        Rectangle bounds = s.getBounds();
-        int width = bounds.width;
-        int height = bounds.height;
-        return decodeGradient(0, height / 2, width - 1, height / 2, new float[] { 0f, 1f }, new Color[] { top, bottom });
-    }
-
     private Shape decodeTrackBackground(int width, int height) {
         rect.setRect(0, 0, width, height);
         return rect;
-    }
-
-    private Shape decodeTopCapPath(int width, int height) {
-        width = 9;
-        height = 15;
-        path.reset();
-        path.moveTo(0, 0);
-        path.lineTo(0, height);
-        path.lineTo(width, height);
-        path.quadTo(width - height / 3.0, height, width - height / 3.0, height / 2.0);
-        path.quadTo(width - height / 3.0, 0, width, 0);
-        path.closePath();
-        return path;
-    }
-
-    /**
-     * Customized Nimbus's drop shadow effect for text fields.
-     */
-    private static class ScrollButtonDropShadowEffect extends DropShadowEffect {
-
-        public ScrollButtonDropShadowEffect() {
-            color = new Color(150, 150, 150);
-            angle = 0;
-            distance = 0;
-            size = 3;
-            opacity = 0.25f;
-        }
-    }
-
-    private static class ButtonStateColors {
-        public Color top;
-        public Color bottom;
-        public Color line;
-
-        public ButtonStateColors(Color top, Color bottom, Color line) {
-            this.top = top;
-            this.bottom = bottom;
-            this.line = line;
-        }
     }
 }
