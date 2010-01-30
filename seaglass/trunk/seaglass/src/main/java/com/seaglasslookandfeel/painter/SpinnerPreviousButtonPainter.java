b843123b9ebaaa41b73026632cea7990c95285af
kathryn <kathryn@kathrynhuxtable.org>
2010-01-30 23:36:32 +0000

    Abstracted out color knowledge.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/SpinnerPreviousButtonPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/SpinnerPreviousButtonPainter.java
@@ -19,16 +19,16 @@
  */
 package com.seaglasslookandfeel.painter;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Paint;
 import java.awt.Shape;
-import java.awt.geom.Rectangle2D;
 
 import javax.swing.JComponent;
 
 import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
+import com.seaglasslookandfeel.painter.util.ColorUtil;
 import com.seaglasslookandfeel.painter.util.ShapeUtil;
+import com.seaglasslookandfeel.painter.util.ColorUtil.ButtonType;
+import com.seaglasslookandfeel.painter.util.ColorUtil.FocusType;
 import com.seaglasslookandfeel.painter.util.ShapeUtil.CornerSize;
 import com.seaglasslookandfeel.painter.util.ShapeUtil.CornerStyle;
 
@@ -49,67 +49,26 @@ public final class SpinnerPreviousButtonPainter extends AbstractRegionPainter {
         FOREGROUND_PRESSED,
     }
 
-    private Color        outerFocusColor          = decodeColor("seaGlassOuterFocus");
-    private Color        innerFocusColor          = decodeColor("seaGlassFocus");
-    private Color        outerToolBarFocusColor   = decodeColor("seaGlassToolBarOuterFocus");
-    private Color        innerToolBarFocusColor   = decodeColor("seaGlassToolBarFocus");
-
-    private Color        DISABLED_TOP_INTERIOR    = new Color(0xd8dbe4f0, true);
-    private Color        DISABLED_BOTTOM_INTERIOR = new Color(0xdddae5f0, true);
-    private Color        DISABLED_TOP_LINE        = new Color(0xe0e4ebf3, true);
-    private Color        DISABLED_TOP_BORDER      = new Color(0x807aa1d4, true);
-    private Color        DISABLED_BOTTOM_BORDER   = new Color(0x805987c0, true);
-
-    private Color        ENABLED_TOP_INTERIOR     = new Color(0x81aed4);
-    private Color        ENABLED_BOTTOM_INTERIOR  = new Color(0xaad4f1);
-    private Color        ENABLED_TOP_LINE         = new Color(0xacc8e0);
-    private Color        ENABLED_TOP_BORDER       = new Color(0x4778bf);
-    private Color        ENABLED_BOTTOM_BORDER    = new Color(0x4076bf);
-
-    private Color        PRESSED_TOP_INTERIOR     = new Color(0x6c91b8);
-    private Color        PRESSED_BOTTOM_INTERIOR  = new Color(0x9cc3de);
-    private Color        PRESSED_TOP_LINE         = new Color(0x9eb6cf);
-    private Color        PRESSED_TOP_BORDER       = new Color(0x4778bf);
-    private Color        PRESSED_BOTTOM_BORDER    = new Color(0x4076bf);
-
-    private Color        ENABLED_ARROW_COLOR      = new Color(0x000000);
-    private Color        DISABLED_ARROW_COLOR     = new Color(0x9ba8cf);
-
-    private Which        state;
     private PaintContext ctx;
+    private ButtonType   type;
     private boolean      focused;
+    private boolean      isForeground;
 
     public SpinnerPreviousButtonPainter(Which state) {
         super();
-        this.state = state;
         this.ctx = new PaintContext(CacheMode.FIXED_SIZES);
 
+        type = getButtonType(state);
         focused = (state == Which.BACKGROUND_FOCUSED || state == Which.BACKGROUND_PRESSED_FOCUSED);
+        isForeground = isForeground(state);
     }
 
     @Override
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
-        switch (state) {
-        case BACKGROUND_DISABLED:
-            paintButtonDisabled(g, c, width, height);
-            break;
-        case BACKGROUND_ENABLED:
-        case BACKGROUND_FOCUSED:
-            paintButtonEnabled(g, c, width, height);
-            break;
-        case BACKGROUND_PRESSED_FOCUSED:
-        case BACKGROUND_PRESSED:
-            paintButtonPressed(g, c, width, height);
-            break;
-        case FOREGROUND_DISABLED:
-            paintArrowDisabled(g, c, width, height);
-            break;
-        case FOREGROUND_ENABLED:
-        case FOREGROUND_FOCUSED:
-        case FOREGROUND_PRESSED_FOCUSED:
-        case FOREGROUND_PRESSED:
-            paintArrowEnabled(g, c, width, height);
-            break;
+        if (isForeground) {
+            paintArrow(g, width, height);
+        } else {
+            paintButton(g, c, width, height);
         }
     }
 
@@ -118,79 +77,73 @@ public final class SpinnerPreviousButtonPainter extends AbstractRegionPainter {
         return ctx;
     }
 
-    private void paintButtonDisabled(Graphics2D g, JComponent c, int width, int height) {
-        paintButton(g, c, width, height, DISABLED_TOP_BORDER, DISABLED_BOTTOM_BORDER, DISABLED_TOP_LINE, DISABLED_TOP_INTERIOR,
-            DISABLED_BOTTOM_INTERIOR);
+    private ButtonType getButtonType(Which state) {
+        switch (state) {
+        case BACKGROUND_DISABLED:
+        case FOREGROUND_DISABLED:
+            return ButtonType.DISABLED;
+        case BACKGROUND_ENABLED:
+        case BACKGROUND_FOCUSED:
+        case FOREGROUND_ENABLED:
+        case FOREGROUND_FOCUSED:
+            return ButtonType.ENABLED;
+        case BACKGROUND_PRESSED_FOCUSED:
+        case BACKGROUND_PRESSED:
+        case FOREGROUND_PRESSED_FOCUSED:
+        case FOREGROUND_PRESSED:
+            return ButtonType.PRESSED;
+        }
+        return null;
     }
 
-    private void paintButtonEnabled(Graphics2D g, JComponent c, int width, int height) {
-        paintButton(g, c, width, height, ENABLED_TOP_BORDER, ENABLED_BOTTOM_BORDER, ENABLED_TOP_LINE, ENABLED_TOP_INTERIOR,
-            ENABLED_BOTTOM_INTERIOR);
+    private boolean isForeground(Which state) {
+        switch (state) {
+        case FOREGROUND_DISABLED:
+        case FOREGROUND_ENABLED:
+        case FOREGROUND_FOCUSED:
+        case FOREGROUND_PRESSED_FOCUSED:
+        case FOREGROUND_PRESSED:
+            return true;
+        default:
+            return false;
+        }
     }
 
-    private void paintButtonPressed(Graphics2D g, JComponent c, int width, int height) {
-        paintButton(g, c, width, height, PRESSED_TOP_BORDER, PRESSED_BOTTOM_BORDER, PRESSED_TOP_LINE, PRESSED_TOP_INTERIOR,
-            PRESSED_BOTTOM_INTERIOR);
-    }
-
-    private void paintArrowDisabled(Graphics2D g, JComponent c, int width, int height) {
-        paintArrow(g, width, height, DISABLED_ARROW_COLOR);
-    }
-
-    private void paintArrowEnabled(Graphics2D g, JComponent c, int width, int height) {
-        paintArrow(g, width, height, ENABLED_ARROW_COLOR);
-    }
-
-    private void paintButton(Graphics2D g, JComponent c, int width, int height, Color topBorder, Color bottomBorder, Color topLine,
-        Color topInterior, Color bottomInterior) {
+    private void paintButton(Graphics2D g, JComponent c, int width, int height) {
         boolean useToolBarColors = isInToolBar(c);
         Shape s;
 
         if (focused) {
-            s = setButtonShape(0, 0, width, height, CornerSize.OUTER_FOCUS);
-            g.setColor(useToolBarColors ? outerToolBarFocusColor : outerFocusColor);
-            g.fill(s);
+            s = createButtonShape(0, 0, width, height, CornerSize.OUTER_FOCUS);
+            ColorUtil.fillFocus(g, s, FocusType.OUTER_FOCUS, useToolBarColors);
 
-            s = setButtonShape(0, 0, width - 1, height - 1, CornerSize.INNER_FOCUS);
-            g.setColor(useToolBarColors ? innerToolBarFocusColor : innerFocusColor);
-            g.fill(s);
+            s = createButtonShape(0, 0, width - 1, height - 1, CornerSize.INNER_FOCUS);
+            ColorUtil.fillFocus(g, s, FocusType.INNER_FOCUS, useToolBarColors);
         }
 
-        s = setButtonShape(0, 0, width - 2, height - 2, CornerSize.BORDER);
-        g.setPaint(decodeGradient(s, topBorder, bottomBorder));
-        g.fill(s);
+        s = createButtonShape(0, 0, width - 2, height - 2, CornerSize.BORDER);
+        ColorUtil.fillSpinnerPrevBorderColors(g, s, type);
 
-        s = setButtonShape(1, 1, width - 4, height - 4, CornerSize.INTERIOR);
-        g.setPaint(decodeGradient(s, topInterior, bottomInterior));
-        g.fill(s);
+        s = createButtonShape(1, 1, width - 4, height - 4, CornerSize.INTERIOR);
+        ColorUtil.fillSpinnerPrevInteriorColors(g, s, type);
 
-        g.setColor(topLine);
-        g.drawLine(1, 0, width - 4, 0);
+        s = ShapeUtil.createRectangle(1, 0, width - 4, 1);
+        ColorUtil.fillSpinnerPrevTopLineColors(g, s, type);
     }
 
-    private void paintArrow(Graphics2D g, int width, int height, Color color) {
-        Shape s = setArrowShape(width, height);
-        g.setColor(color);
-        g.fill(s);
+    private void paintArrow(Graphics2D g, int width, int height) {
+        Shape s = createArrowShape(width, height);
+        ColorUtil.fillSpinnerArrowColors(g, s, type);
     }
 
-    private Shape setButtonShape(int x, int y, int width, int height, CornerSize size) {
+    private Shape createButtonShape(int x, int y, int width, int height, CornerSize size) {
         return ShapeUtil.createRoundRectangle(x, y, width, height, size, CornerStyle.SQUARE, CornerStyle.SQUARE, CornerStyle.ROUNDED,
             CornerStyle.SQUARE);
     }
 
-    private Shape setArrowShape(int left, int height) {
+    private Shape createArrowShape(int left, int height) {
         int centerX = 8;
         int centerY = height / 2;
         return ShapeUtil.createArrowDown(centerX, centerY - 2, 4, 3);
-    }
-
-    private Paint decodeGradient(Shape s, Color color1, Color color2) {
-        Rectangle2D bounds = s.getBounds2D();
-        float x = (float) bounds.getX();
-        float y = (float) bounds.getY();
-        float w = (float) bounds.getWidth();
-        float h = (float) bounds.getHeight();
-        return decodeGradient((0.5f * w) + x, y, (0.5f * w) + x, h + y, new float[] { 0f, 1f }, new Color[] { color1, color2 });
     }
 }
