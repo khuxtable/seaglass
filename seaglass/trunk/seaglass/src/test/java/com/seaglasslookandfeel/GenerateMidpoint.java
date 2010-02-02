e9b2db4b9238cfac79e89e0c192fc12f4634e3fd
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2010-02-02 13:54:22 +0000

    Allow colors to begin with 0x so that they can be pasted from Eclipse from a simple double-click selection.

--- seaglass/trunk/seaglass/src/test/java/com/seaglasslookandfeel/GenerateMidpoint.java
+++ seaglass/trunk/seaglass/src/test/java/com/seaglasslookandfeel/GenerateMidpoint.java
@@ -6,17 +6,15 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-public class GenerateDifferences {
+public class GenerateMidpoint {
 
     private static JTextField c1;
     private static JTextField c2;
@@ -31,11 +29,6 @@ public class GenerateDifferences {
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                JPopupMenu popupMenu = new JPopupMenu();
-                popupMenu.add("First item");
-                popupMenu.add("Second item");
-                popupMenu.add("Third item");
-
                 c1 = new JTextField();
                 c1.setPreferredSize(new Dimension(180, 25));
                 c2 = new JTextField();
@@ -49,19 +42,18 @@ public class GenerateDifferences {
                         String c2Str = c2.getText();
                         if (c1Str.startsWith("0x")) c1Str = c1Str.substring(2);
                         if (c2Str.startsWith("0x")) c2Str = c2Str.substring(2);
-                        Color src = new Color(Integer.parseInt(c1Str, 16));
-                        Color dest = new Color(Integer.parseInt(c2Str, 16));
-                        result.setText(calcOffsets(src, dest));
+                        Color color1 = new Color(Integer.parseInt(c1Str, 16));
+                        Color color2 = new Color(Integer.parseInt(c2Str, 16));
+                        result.setText(deriveColor(color1, color2));
                     }
                 });
                 calc.setText("Calculate");
 
                 result = new JTextPane();
-                result.setPreferredSize(new Dimension(300, 100));
+                result.setPreferredSize(new Dimension(180, 100));
                 result.setEditable(false);
 
                 JPanel panel = new JPanel();
-                panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
                 panel.setBackground(new Color(248, 248, 248));
                 panel.add(c1);
                 panel.add(c2);
@@ -70,7 +62,7 @@ public class GenerateDifferences {
 
                 JFrame frame = new JFrame("Title");
                 frame.add(panel, BorderLayout.CENTER);
-                frame.setSize(325, 225);
+                frame.setSize(275, 225);
                 frame.setLocationRelativeTo(null);
                 frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 frame.setVisible(true);
@@ -78,15 +70,16 @@ public class GenerateDifferences {
         });
     }
 
-    private static String calcOffsets(Color src, Color dest) {
-        float[] tmp1 = Color.RGBtoHSB(src.getRed(), src.getGreen(), src.getBlue(), null);
-        float[] tmp2 = Color.RGBtoHSB(dest.getRed(), dest.getGreen(), dest.getBlue(), null);
+    private static String deriveColor(Color color1, Color color2) {
+        int dest = deriveARGB(color1, color2, 0.5f);
+        return String.format("%06x", dest & 0xFFFFFF);
+    }
 
-        // determine offsets
-        float hOffset = tmp2[0] - tmp1[0];
-        float sOffset = tmp2[1] - tmp1[1];
-        float bOffset = tmp2[2] - tmp1[2];
-
-        return String.format("deriveColor(src, %ff, %ff, %ff, 0)", hOffset, sOffset, bOffset);
+    private static int deriveARGB(Color color1, Color color2, float midPoint) {
+        int r = color1.getRed() + (int) ((color2.getRed() - color1.getRed()) * midPoint + 0.5f);
+        int g = color1.getGreen() + (int) ((color2.getGreen() - color1.getGreen()) * midPoint + 0.5f);
+        int b = color1.getBlue() + (int) ((color2.getBlue() - color1.getBlue()) * midPoint + 0.5f);
+        int a = color1.getAlpha() + (int) ((color2.getAlpha() - color1.getAlpha()) * midPoint + 0.5f);
+        return ((a & 0xFF) << 24) | ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
     }
 }
