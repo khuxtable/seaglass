9210c5fc0cfb1e3e48404eb562cd89858ee13aed
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2010-02-03 16:05:18 +0000

    Factored inner shadow code into the Effect class.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/effect/SeaGlassInternalShadowEffect.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/effect/SeaGlassInternalShadowEffect.java
@@ -19,14 +19,27 @@
  */
 package com.seaglasslookandfeel.effect;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.LinearGradientPaint;
+import java.awt.Paint;
 import java.awt.Rectangle;
 import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
 
-import com.seaglasslookandfeel.painter.util.PaintUtil;
+import javax.swing.UIManager;
+
 import com.seaglasslookandfeel.painter.util.ShapeUtil;
+import com.seaglasslookandfeel.painter.util.PaintUtil.TwoColors;
 
 public class SeaGlassInternalShadowEffect {
+
+    private Color     transparentColor = UIManager.getColor("seaGlassTransparent");
+    private Color     innerShadowBase  = UIManager.getColor("seaGlassInnerShadow");
+
+    private Color     innerShadowLight = new Color(innerShadowBase.getRed(), innerShadowBase.getGreen(), innerShadowBase.getBlue(),
+                                           innerShadowBase.getAlpha() / 2);
+    private TwoColors innerShadow      = new TwoColors(innerShadowBase, innerShadowLight);
 
     public void fill(Graphics2D g, Shape s, boolean isRounded, boolean paintRightShadow) {
         if (isRounded) {
@@ -44,23 +57,63 @@ public class SeaGlassInternalShadowEffect {
         int h = bounds.height;
 
         s = ShapeUtil.createRectangle(x, y, w, 2);
-        g.setPaint(PaintUtil.getTopShadowPaint(s));
+        g.setPaint(getTopShadowPaint(s));
         g.fill(s);
 
         s = ShapeUtil.createRectangle(x, y, 1, h);
-        g.setPaint(PaintUtil.getLeftShadowPaint(s));
+        g.setPaint(getLeftShadowPaint(s));
         g.fill(s);
 
         if (paintRightShadow) {
             s = ShapeUtil.createRectangle(x + w - 1, y, 1, h);
-            g.setPaint(PaintUtil.getRightShadowPaint(s));
+            g.setPaint(getRightShadowPaint(s));
             g.fill(s);
         }
     }
 
     private void fillInternalShadowRounded(Graphics2D g, Shape s) {
-        g.setPaint(PaintUtil.getRoundedShadowPaint(s));
+        g.setPaint(getRoundedShadowPaint(s));
         g.fill(s);
     }
 
+    public Paint getRoundedShadowPaint(Shape s) {
+        Rectangle r = s.getBounds();
+        int x = r.x + r.width / 2;
+        int y1 = r.y;
+        float frac = 1.0f / r.height;
+        int y2 = r.y + r.height;
+        return new LinearGradientPaint(x, y1, x, y2, (new float[] { 0f, frac, 1f }), (new Color[] {
+            innerShadow.top,
+            innerShadow.bottom,
+            innerShadow.bottom }));
+    }
+
+    public Paint getTopShadowPaint(Shape s) {
+        Rectangle2D bounds = s.getBounds2D();
+        float minY = (float) bounds.getMinY();
+        float maxY = (float) bounds.getMaxY();
+        float midX = (float) bounds.getCenterX();
+        return new LinearGradientPaint(midX, minY, midX, maxY, (new float[] { 0f, 1f }),
+            (new Color[] { innerShadow.top, transparentColor }));
+    }
+
+    public Paint getLeftShadowPaint(Shape s) {
+        Rectangle2D bounds = s.getBounds2D();
+        float minX = (float) bounds.getMinX();
+        float maxX = (float) bounds.getMaxX();
+        float midY = (float) bounds.getCenterY();
+        return new LinearGradientPaint(minX, midY, maxX, midY, (new float[] { 0f, 1f }), (new Color[] {
+            innerShadow.bottom,
+            transparentColor }));
+    }
+
+    public Paint getRightShadowPaint(Shape s) {
+        Rectangle2D bounds = s.getBounds2D();
+        float minX = (float) bounds.getMinX() - 1;
+        float maxX = (float) bounds.getMaxX() - 1;
+        float midY = (float) bounds.getCenterY();
+        return new LinearGradientPaint(minX, midY, maxX, midY, (new float[] { 0f, 1f }), (new Color[] {
+            transparentColor,
+            innerShadow.bottom }));
+    }
 }
