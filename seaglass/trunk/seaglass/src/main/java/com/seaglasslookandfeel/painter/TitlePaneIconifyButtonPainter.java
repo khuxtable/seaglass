7c31b42e1bd6c1a0e75b475ea86c873fae2c90bd
kathryn <kathryn@kathrynhuxtable.org>
2010-02-07 00:26:40 +0000

    Formatted source with Jalopy.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TitlePaneIconifyButtonPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TitlePaneIconifyButtonPainter.java
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * $Id$
  */
 package com.seaglasslookandfeel.painter;
@@ -26,6 +26,8 @@ import java.awt.RenderingHints;
 import javax.swing.JComponent;
 
 import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
+
+import com.seaglasslookandfeel.painter.titlepaneforegound.TitlePaneButtonForegroundPainter;
 import com.seaglasslookandfeel.painter.titlepaneforegound.TitlePaneIconifyButtonForegroundPainter;
 import com.seaglasslookandfeel.painter.titlepaneforegound.TitlePaneRestoreButtonForegroundPainter;
 
@@ -33,57 +35,60 @@ import com.seaglasslookandfeel.painter.titlepaneforegound.TitlePaneRestoreButton
  * Title pane iconify button painter implementation.
  */
 public final class TitlePaneIconifyButtonPainter extends TitlePaneButtonPainter {
-    public static enum Which {
-        BACKGROUND_ENABLED,
-        BACKGROUND_DISABLED,
-        BACKGROUND_MOUSEOVER,
-        BACKGROUND_PRESSED,
-        BACKGROUND_ENABLED_WINDOWNOTFOCUSED,
-        BACKGROUND_MOUSEOVER_WINDOWNOTFOCUSED,
-        BACKGROUND_PRESSED_WINDOWNOTFOCUSED,
 
-        BACKGROUND_MINIMIZED_DISABLED,
-        BACKGROUND_MINIMIZED_ENABLED,
-        BACKGROUND_MINIMIZED_MOUSEOVER,
-        BACKGROUND_MINIMIZED_PRESSED,
-        BACKGROUND_MINIMIZED_ENABLED_WINDOWNOTFOCUSED,
-        BACKGROUND_MINIMIZED_MOUSEOVER_WINDOWNOTFOCUSED,
+    /**
+     * Control states.
+     */
+    public static enum Which {
+        BACKGROUND_ENABLED, BACKGROUND_DISABLED, BACKGROUND_MOUSEOVER, BACKGROUND_PRESSED, BACKGROUND_ENABLED_WINDOWNOTFOCUSED,
+        BACKGROUND_MOUSEOVER_WINDOWNOTFOCUSED, BACKGROUND_PRESSED_WINDOWNOTFOCUSED,
+
+        BACKGROUND_MINIMIZED_DISABLED, BACKGROUND_MINIMIZED_ENABLED, BACKGROUND_MINIMIZED_MOUSEOVER, BACKGROUND_MINIMIZED_PRESSED,
+        BACKGROUND_MINIMIZED_ENABLED_WINDOWNOTFOCUSED, BACKGROUND_MINIMIZED_MOUSEOVER_WINDOWNOTFOCUSED,
         BACKGROUND_MINIMIZED_PRESSED_WINDOWNOTFOCUSED,
     }
 
-    private ButtonColors                            enabled        = new ButtonColors(white16, black66, white4c, black66, black33, white33,
-                                                                       transparentColor);
-    private ButtonColors                            hover          = new ButtonColors(white68, black66, white8c, black66, black33, white46,
-                                                                       white59);
-    private ButtonColors                            pressed        = new ButtonColors(gray_9b_82, black66, gray_a9_9e, black66, black33,
-                                                                       white33, gray_e6_59);
+    private ButtonColors enabled = new ButtonColors(white16, black66, white4c, black66, black33, white33, transparentColor);
+    private ButtonColors hover   = new ButtonColors(white68, black66, white8c, black66, black33, white46, white59);
+    private ButtonColors pressed = new ButtonColors(gray_9b_82, black66, gray_a9_9e, black66, black33, white33, gray_e6_59);
 
     private TitlePaneIconifyButtonForegroundPainter iconifyPainter = new TitlePaneIconifyButtonForegroundPainter();
-    private TitlePaneRestoreButtonForegroundPainter restorePainter = new TitlePaneRestoreButtonForegroundPainter();
+    private TitlePaneButtonForegroundPainter        restorePainter = new TitlePaneRestoreButtonForegroundPainter();
 
-    private Which                                   state;
-    private PaintContext                            ctx;
+    private Which        state;
+    private PaintContext ctx;
 
+    /**
+     * Creates a new TitlePaneIconifyButtonPainter object.
+     *
+     * @param state the component state.
+     */
     public TitlePaneIconifyButtonPainter(Which state) {
         super();
         this.state = state;
-        this.ctx = new PaintContext(CacheMode.FIXED_SIZES);
+        this.ctx   = new PaintContext(CacheMode.FIXED_SIZES);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
         switch (state) {
+
         case BACKGROUND_ENABLED:
         case BACKGROUND_DISABLED:
         case BACKGROUND_ENABLED_WINDOWNOTFOCUSED:
             paintBackgroundEnabled(g, c, width, height);
             paintMinimizeEnabled(g, c, width, height);
             break;
+
         case BACKGROUND_MOUSEOVER:
         case BACKGROUND_MOUSEOVER_WINDOWNOTFOCUSED:
             paintBackgroundHover(g, c, width, height);
             paintMinimizeHover(g, c, width, height);
             break;
+
         case BACKGROUND_PRESSED:
         case BACKGROUND_PRESSED_WINDOWNOTFOCUSED:
             paintBackgroundPressed(g, c, width, height);
@@ -96,11 +101,13 @@ public final class TitlePaneIconifyButtonPainter extends TitlePaneButtonPainter 
             paintBackgroundEnabled(g, c, width, height);
             paintRestoreEnabled(g, c, width, height);
             break;
+
         case BACKGROUND_MINIMIZED_MOUSEOVER:
         case BACKGROUND_MINIMIZED_MOUSEOVER_WINDOWNOTFOCUSED:
             paintBackgroundHover(g, c, width, height);
             paintRestoreHover(g, c, width, height);
             break;
+
         case BACKGROUND_MINIMIZED_PRESSED:
         case BACKGROUND_MINIMIZED_PRESSED_WINDOWNOTFOCUSED:
             paintBackgroundPressed(g, c, width, height);
@@ -109,47 +116,131 @@ public final class TitlePaneIconifyButtonPainter extends TitlePaneButtonPainter 
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected PaintContext getPaintContext() {
         return ctx;
     }
 
+    /**
+     * Paint the background enabled state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintBackgroundEnabled(Graphics2D g, JComponent c, int width, int height) {
         paintBackground(g, c, width, height, enabled);
     }
 
+    /**
+     * Paint the background mouse-over state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintBackgroundHover(Graphics2D g, JComponent c, int width, int height) {
         paintBackground(g, c, width, height, hover);
     }
 
+    /**
+     * Paint the background pressed state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintBackgroundPressed(Graphics2D g, JComponent c, int width, int height) {
         paintBackground(g, c, width, height, pressed);
     }
 
+    /**
+     * Paint the foreground minimized button enabled state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintMinimizeEnabled(Graphics2D g, JComponent c, int width, int height) {
         iconifyPainter.paintEnabled(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground minimized button mouse-over state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintMinimizeHover(Graphics2D g, JComponent c, int width, int height) {
         iconifyPainter.paintHover(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground minimize button pressed state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintMinimizePressed(Graphics2D g, JComponent c, int width, int height) {
         iconifyPainter.paintPressed(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground restore button enabled state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintRestoreEnabled(Graphics2D g, JComponent c, int width, int height) {
         restorePainter.paintEnabled(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground restore button mouse-over state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintRestoreHover(Graphics2D g, JComponent c, int width, int height) {
         restorePainter.paintHover(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground restore button pressed state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintRestorePressed(Graphics2D g, JComponent c, int width, int height) {
         restorePainter.paintPressed(g, c, width, height);
     }
 
+    /**
+     * Paint the background of the button using the specified colors.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     * @param colors the color set to use to paint the button.
+     */
     private void paintBackground(Graphics2D g, JComponent c, int width, int height, ButtonColors colors) {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
         g.setColor(colors.top);
@@ -172,23 +263,51 @@ public final class TitlePaneIconifyButtonPainter extends TitlePaneButtonPainter 
         g.drawLine(3, height - 3, width - 2, height - 3);
     }
 
+    /**
+     * A class encapsulating the colors used for a button.
+     */
     private static class ButtonColors {
+
+        /** Top line color. */
         public Color top;
+
+        /** Left edge color. */
         public Color leftOuter;
+
+        /** Left edge inside color. */
         public Color leftInner;
+
+        /** Main edge color. */
         public Color edge;
+
+        /** Main edge shade color. */
         public Color edgeShade;
+
+        /** Main shadow color. */
         public Color shadow;
+
+        /** Interior color. */
         public Color interior;
 
+        /**
+         * Creates a new ButtonColors object.
+         *
+         * @param top       the top line color.
+         * @param leftOuter the left edge color.
+         * @param leftInner the left edge inside color.
+         * @param edge      the main edge color.
+         * @param edgeShade the main edge shade color.
+         * @param shadow    the shadow color.
+         * @param interior  the interior color.
+         */
         public ButtonColors(Color top, Color leftOuter, Color leftInner, Color edge, Color edgeShade, Color shadow, Color interior) {
-            this.top = top;
+            this.top       = top;
             this.leftOuter = leftOuter;
             this.leftInner = leftInner;
-            this.edge = edge;
+            this.edge      = edge;
             this.edgeShade = edgeShade;
-            this.shadow = shadow;
-            this.interior = interior;
+            this.shadow    = shadow;
+            this.interior  = interior;
         }
 
     }
--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TitlePaneMaximizeButtonPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TitlePaneMaximizeButtonPainter.java
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * $Id$
  */
 package com.seaglasslookandfeel.painter;
@@ -26,63 +26,71 @@ import java.awt.RenderingHints;
 import javax.swing.JComponent;
 
 import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
+
+import com.seaglasslookandfeel.painter.titlepaneforegound.TitlePaneButtonForegroundPainter;
 import com.seaglasslookandfeel.painter.titlepaneforegound.TitlePaneMaximizeButtonForegroundPainter;
 import com.seaglasslookandfeel.painter.titlepaneforegound.TitlePaneRestoreButtonForegroundPainter;
 
 /**
  * Title pane maximize/minimize button (aka "toggleButton") painter.
+ *
+ * @author Kathryn Huxtable
  */
 public final class TitlePaneMaximizeButtonPainter extends TitlePaneButtonPainter {
-    public static enum Which {
-        BACKGROUND_DISABLED,
-        BACKGROUND_ENABLED,
-        BACKGROUND_MOUSEOVER,
-        BACKGROUND_PRESSED,
-        BACKGROUND_ENABLED_WINDOWNOTFOCUSED,
-        BACKGROUND_MOUSEOVER_WINDOWNOTFOCUSED,
-        BACKGROUND_PRESSED_WINDOWNOTFOCUSED,
 
-        BACKGROUND_MAXIMIZED_DISABLED,
-        BACKGROUND_MAXIMIZED_ENABLED,
-        BACKGROUND_MAXIMIZED_MOUSEOVER,
-        BACKGROUND_MAXIMIZED_PRESSED,
-        BACKGROUND_MAXIMIZED_ENABLED_WINDOWNOTFOCUSED,
-        BACKGROUND_MAXIMIZED_MOUSEOVER_WINDOWNOTFOCUSED,
+    /**
+     * Control states.
+     */
+    public static enum Which {
+        BACKGROUND_DISABLED, BACKGROUND_ENABLED, BACKGROUND_MOUSEOVER, BACKGROUND_PRESSED, BACKGROUND_ENABLED_WINDOWNOTFOCUSED,
+        BACKGROUND_MOUSEOVER_WINDOWNOTFOCUSED, BACKGROUND_PRESSED_WINDOWNOTFOCUSED,
+
+        BACKGROUND_MAXIMIZED_DISABLED, BACKGROUND_MAXIMIZED_ENABLED, BACKGROUND_MAXIMIZED_MOUSEOVER, BACKGROUND_MAXIMIZED_PRESSED,
+        BACKGROUND_MAXIMIZED_ENABLED_WINDOWNOTFOCUSED, BACKGROUND_MAXIMIZED_MOUSEOVER_WINDOWNOTFOCUSED,
         BACKGROUND_MAXIMIZED_PRESSED_WINDOWNOTFOCUSED,
     }
 
-    private ButtonColors                             enabled         = new ButtonColors(white16, white4c, black66, white33,
-                                                                         transparentColor);
-    private ButtonColors                             hover           = new ButtonColors(white68, white8c, black66, white33, white59);
-    private ButtonColors                             pressed         = new ButtonColors(gray_9b_82, gray_a9_9e, black66, white33,
-                                                                         gray_e6_59);
+    private ButtonColors enabled = new ButtonColors(white16, white4c, black66, white33, transparentColor);
+    private ButtonColors hover   = new ButtonColors(white68, white8c, black66, white33, white59);
+    private ButtonColors pressed = new ButtonColors(gray_9b_82, gray_a9_9e, black66, white33, gray_e6_59);
 
     private TitlePaneMaximizeButtonForegroundPainter maximizePainter = new TitlePaneMaximizeButtonForegroundPainter();
-    private TitlePaneRestoreButtonForegroundPainter  restorePainter  = new TitlePaneRestoreButtonForegroundPainter();
+    private TitlePaneButtonForegroundPainter         restorePainter  = new TitlePaneRestoreButtonForegroundPainter();
 
-    private Which                                    state;
-    private PaintContext                             ctx;
+    private Which        state;
+    private PaintContext ctx;
 
+    /**
+     * Creates a new TitlePaneMaximizeButtonPainter object.
+     *
+     * @param state the button state.
+     */
     public TitlePaneMaximizeButtonPainter(Which state) {
         super();
         this.state = state;
-        this.ctx = new PaintContext(CacheMode.FIXED_SIZES);
+        this.ctx   = new PaintContext(CacheMode.FIXED_SIZES);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
         switch (state) {
+
         case BACKGROUND_DISABLED:
         case BACKGROUND_ENABLED:
         case BACKGROUND_ENABLED_WINDOWNOTFOCUSED:
             paintBackgroundEnabled(g, c, width, height);
             paintMaximizeEnabled(g, c, width, height);
             break;
+
         case BACKGROUND_MOUSEOVER:
         case BACKGROUND_MOUSEOVER_WINDOWNOTFOCUSED:
             paintBackgroundHover(g, c, width, height);
             paintMaximizeHover(g, c, width, height);
             break;
+
         case BACKGROUND_PRESSED:
         case BACKGROUND_PRESSED_WINDOWNOTFOCUSED:
             paintBackgroundPressed(g, c, width, height);
@@ -95,11 +103,13 @@ public final class TitlePaneMaximizeButtonPainter extends TitlePaneButtonPainter
             paintBackgroundEnabled(g, c, width, height);
             paintRestoreEnabled(g, c, width, height);
             break;
+
         case BACKGROUND_MAXIMIZED_MOUSEOVER:
         case BACKGROUND_MAXIMIZED_MOUSEOVER_WINDOWNOTFOCUSED:
             paintBackgroundHover(g, c, width, height);
             paintRestoreHover(g, c, width, height);
             break;
+
         case BACKGROUND_MAXIMIZED_PRESSED:
         case BACKGROUND_MAXIMIZED_PRESSED_WINDOWNOTFOCUSED:
             paintBackgroundPressed(g, c, width, height);
@@ -108,47 +118,131 @@ public final class TitlePaneMaximizeButtonPainter extends TitlePaneButtonPainter
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected PaintContext getPaintContext() {
         return ctx;
     }
 
+    /**
+     * Paint the background enabled state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintBackgroundEnabled(Graphics2D g, JComponent c, int width, int height) {
         paintBackground(g, c, width, height, enabled);
     }
 
+    /**
+     * Paint the background mouse-over state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintBackgroundHover(Graphics2D g, JComponent c, int width, int height) {
         paintBackground(g, c, width, height, hover);
     }
 
+    /**
+     * Paint the background pressed state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintBackgroundPressed(Graphics2D g, JComponent c, int width, int height) {
         paintBackground(g, c, width, height, pressed);
     }
 
+    /**
+     * Paint the foreground maximized button enabled state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintMaximizeEnabled(Graphics2D g, JComponent c, int width, int height) {
         maximizePainter.paintEnabled(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground maximized button mouse-over state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintMaximizeHover(Graphics2D g, JComponent c, int width, int height) {
         maximizePainter.paintHover(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground maximize button pressed state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintMaximizePressed(Graphics2D g, JComponent c, int width, int height) {
         maximizePainter.paintPressed(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground restore button enabled state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintRestoreEnabled(Graphics2D g, JComponent c, int width, int height) {
         restorePainter.paintEnabled(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground restore button mouse-over state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintRestoreHover(Graphics2D g, JComponent c, int width, int height) {
         restorePainter.paintHover(g, c, width, height);
     }
 
+    /**
+     * Paint the foreground restore button pressed state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintRestorePressed(Graphics2D g, JComponent c, int width, int height) {
         restorePainter.paintPressed(g, c, width, height);
     }
 
+    /**
+     * Paint the background of the button using the specified colors.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     * @param colors the color set to use to paint the button.
+     */
     private void paintBackground(Graphics2D g, JComponent c, int width, int height, ButtonColors colors) {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
         g.setColor(colors.top);
@@ -164,18 +258,40 @@ public final class TitlePaneMaximizeButtonPainter extends TitlePaneButtonPainter
         g.fillRect(1, 1, width - 1, height - 2);
     }
 
+    /**
+     * A class encapsulating the colors used for a button.
+     */
     private static class ButtonColors {
+
+        /** Top line color. */
         public Color top;
+
+        /** Left edge color. */
         public Color left;
+
+        /** Main edge color. */
         public Color edge;
+
+        /** Main shadow color. */
         public Color shadow;
+
+        /** Interior color. */
         public Color interior;
 
+        /**
+         * Creates a new ButtonColors object.
+         *
+         * @param top      DOCUMENT ME!
+         * @param left     DOCUMENT ME!
+         * @param edge     DOCUMENT ME!
+         * @param shadow   DOCUMENT ME!
+         * @param interior DOCUMENT ME!
+         */
         public ButtonColors(Color top, Color left, Color edge, Color shadow, Color interior) {
-            this.top = top;
-            this.left = left;
-            this.edge = edge;
-            this.shadow = shadow;
+            this.top      = top;
+            this.left     = left;
+            this.edge     = edge;
+            this.shadow   = shadow;
             this.interior = interior;
         }
 
