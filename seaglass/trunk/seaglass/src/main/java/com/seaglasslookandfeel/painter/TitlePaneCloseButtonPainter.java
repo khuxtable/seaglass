b6c3c1a65b9920b6d6b302c138b76e7f72549173
kathryn <kathryn@kathrynhuxtable.org>
2010-02-07 00:55:42 +0000

    Formatted source with Jalopy.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TitlePaneCloseButtonPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TitlePaneCloseButtonPainter.java
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * $Id$
  */
 package com.seaglasslookandfeel.painter;
@@ -33,47 +33,60 @@ import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheM
 
 /**
  * Title pane close button implementation.
+ *
+ * @author Kathryn Huxtable
  */
 public final class TitlePaneCloseButtonPainter extends TitlePaneButtonPainter {
+
+    /**
+     * Control state.
+     */
     public static enum Which {
-        BACKGROUND_DISABLED,
-        BACKGROUND_ENABLED,
-        BACKGROUND_MOUSEOVER,
-        BACKGROUND_PRESSED,
-        BACKGROUND_ENABLED_WINDOWNOTFOCUSED,
+        BACKGROUND_DISABLED, BACKGROUND_ENABLED, BACKGROUND_MOUSEOVER, BACKGROUND_PRESSED, BACKGROUND_ENABLED_WINDOWNOTFOCUSED,
         BACKGROUND_PRESSED_WINDOWNOTFOCUSED,
     }
 
     private ButtonColors enabled = new ButtonColors(white16, white4c, black66, white33, transparentColor, transparentColor, black99,
-                                     white99);
+            white99);
     private ButtonColors hover   = new ButtonColors(closeButtonTopHover, closeButtonLeftHover, closeButtonEdgeHover, white33,
-                                     closeButtonInteriorTopHover, closeButtonInteriorBottomHover, closeButtonMarkBorderHover, white);
+            closeButtonInteriorTopHover, closeButtonInteriorBottomHover, closeButtonMarkBorderHover, white);
     private ButtonColors pressed = new ButtonColors(closeButtonTopPressed, closeButtonLeftPressed, black66, white33,
-                                     closeButtonInteriorTopPressed, closeButtonInteriorTopPressed, closeButtonMarkBorderPressed,
-                                     closeButtonMarkInteriorPressed);
+            closeButtonInteriorTopPressed, closeButtonInteriorTopPressed, closeButtonMarkBorderPressed,
+            closeButtonMarkInteriorPressed);
 
-    private MyPath2D     path    = new MyPath2D();
+    private MyPath2D path = new MyPath2D();
 
     private Which        state;
     private PaintContext ctx;
 
+    /**
+     * Creates a new TitlePaneCloseButtonPainter object.
+     *
+     * @param state the button state.
+     */
     public TitlePaneCloseButtonPainter(Which state) {
         super();
         this.state = state;
-        this.ctx = new PaintContext(CacheMode.FIXED_SIZES);
+        this.ctx   = new PaintContext(CacheMode.FIXED_SIZES);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
         switch (state) {
+
         case BACKGROUND_DISABLED:
         case BACKGROUND_ENABLED:
         case BACKGROUND_ENABLED_WINDOWNOTFOCUSED:
             paintCloseEnabled(g, c, width, height);
             break;
+
         case BACKGROUND_MOUSEOVER:
             paintCloseHover(g, c, width, height);
             break;
+
         case BACKGROUND_PRESSED:
         case BACKGROUND_PRESSED_WINDOWNOTFOCUSED:
             paintClosePressed(g, c, width, height);
@@ -81,27 +94,64 @@ public final class TitlePaneCloseButtonPainter extends TitlePaneButtonPainter {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected PaintContext getPaintContext() {
         return ctx;
     }
 
+    /**
+     * Paint the background enabled state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintCloseEnabled(Graphics2D g, JComponent c, int width, int height) {
         paintClose(g, c, width, height, enabled);
     }
 
+    /**
+     * Paint the background mouse-over state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintCloseHover(Graphics2D g, JComponent c, int width, int height) {
         paintClose(g, c, width, height, hover);
     }
 
+    /**
+     * Paint the background pressed state.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     */
     private void paintClosePressed(Graphics2D g, JComponent c, int width, int height) {
         paintClose(g, c, width, height, pressed);
     }
 
+    /**
+     * Paint the background of the button using the specified colors.
+     *
+     * @param g      the Graphics2D context to paint with.
+     * @param c      the component.
+     * @param width  the width of the component.
+     * @param height the height of the component.
+     * @param colors the color set to use to paint the button.
+     */
     private void paintClose(Graphics2D g, JComponent c, int width, int height, ButtonColors colors) {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         Shape s = decodeInterior(width, height);
-        g.setPaint(decodeCloseGradient(g, s, colors.interiorTop, colors.interiorBottom));
+
+        g.setPaint(decodeCloseGradient(s, colors.interiorTop, colors.interiorBottom));
         g.fill(s);
         s = decodeEdge(width, height);
         g.setColor(colors.edge);
@@ -122,13 +172,31 @@ public final class TitlePaneCloseButtonPainter extends TitlePaneButtonPainter {
         g.draw(s);
     }
 
-    private Paint decodeCloseGradient(Graphics2D g, Shape s, Color top, Color bottom) {
-        Rectangle r = s.getBounds();
-        int width = r.width;
-        int height = r.height;
+    /**
+     * Create the gradient for the close button.
+     *
+     * @param  s      the shape to fill.
+     * @param  top    the top color.
+     * @param  bottom the bottom color.
+     *
+     * @return the gradient.
+     */
+    private Paint decodeCloseGradient(Shape s, Color top, Color bottom) {
+        Rectangle r      = s.getBounds();
+        int       width  = r.width;
+        int       height = r.height;
+
         return createGradient(r.x + width / 2, r.y, r.x + width / 2, r.y + height - 1, new float[] { 0f, 1f }, new Color[] { top, bottom });
     }
 
+    /**
+     * Create the interior of the button.
+     *
+     * @param  width  the width.
+     * @param  height the height.
+     *
+     * @return the shape.
+     */
     private Shape decodeInterior(int width, int height) {
         path.reset();
         path.moveTo(1, 1);
@@ -137,30 +205,58 @@ public final class TitlePaneCloseButtonPainter extends TitlePaneButtonPainter {
         path.lineTo(width - 3, height - 2);
         path.lineTo(1, height - 2);
         path.closePath();
+
         return path;
     }
 
+    /**
+     * Create the edge of the button.
+     *
+     * @param  width  the width.
+     * @param  height the height.
+     *
+     * @return the shape of the edge.
+     */
     private Shape decodeEdge(int width, int height) {
         path.reset();
         path.moveTo(width - 2, 0);
         path.lineTo(width - 2, height - 4);
         path.lineTo(width - 4, height - 2);
         path.lineTo(0, height - 2);
+
         return path;
     }
 
+    /**
+     * Create the shadow for the button.
+     *
+     * @param  width  the width.
+     * @param  height the height.
+     *
+     * @return the shape of the shadow.
+     */
     private Shape decodeShadow(int width, int height) {
         path.reset();
         path.moveTo(width - 1, 0);
         path.lineTo(width - 1, height - 4);
         path.lineTo(width - 4, height - 1);
         path.lineTo(0, height - 1);
+
         return path;
     }
 
+    /**
+     * Create the shape for the mark border.
+     *
+     * @param  width  the width.
+     * @param  height the height.
+     *
+     * @return the shape of the mark border.
+     */
     private Shape decodeMarkBorder(int width, int height) {
         int left = (width - 3) / 2 - 5;
-        int top = (height - 2) / 2 - 5;
+        int top  = (height - 2) / 2 - 5;
+
         path.reset();
         path.moveTo(left + 1, top + 0);
         path.lineTo(left + 3, top + 0);
@@ -192,12 +288,22 @@ public final class TitlePaneCloseButtonPainter extends TitlePaneButtonPainter {
         path.pointAt(left + 2, top + 3);
         path.pointAt(left + 1, top + 2);
         path.pointAt(left + 0, top + 1);
+
         return path;
     }
 
+    /**
+     * Create the shape for the mark interior.
+     *
+     * @param  width  the width.
+     * @param  height the height.
+     *
+     * @return the shape of the mark interior.
+     */
     private Shape decodeMarkInterior(int width, int height) {
         int left = (width - 3) / 2 - 5;
-        int top = (height - 2) / 2 - 5;
+        int top  = (height - 2) / 2 - 5;
+
         path.reset();
         path.moveTo(left + 1, top + 1);
         path.lineTo(left + 4, top + 1);
@@ -214,36 +320,79 @@ public final class TitlePaneCloseButtonPainter extends TitlePaneButtonPainter {
         path.lineTo(left + 4, top + 5);
         path.lineTo(left + 4, top + 4);
         path.closePath();
+
         return path;
     }
 
+    /**
+     * Extend Path2D to have a pointAt method to draw a single point.
+     */
     private static class MyPath2D extends Path2D.Double {
+        private static final long serialVersionUID = -3577694636085901945L;
+
+        /**
+         * Draw a single point at the (x,y) coordinate specified.
+         *
+         * @param x the x coordinate to draw the point at.
+         * @param y the y coordinate to draw the point at.
+         */
         public void pointAt(int x, int y) {
             moveTo(x, y);
             lineTo(x, y);
         }
     }
 
+    /**
+     * A class encapsulating the colors used for a button.
+     */
     private static class ButtonColors {
+
+        /** Top line color. */
         public Color top;
+
+        /** Left edge color. */
         public Color left;
+
+        /** Main edge color. */
         public Color edge;
+
+        /** Main shadow color. */
         public Color shadow;
+
+        /** Interior top gradient color. */
         public Color interiorTop;
+
+        /** Interior bottom gradient color. */
         public Color interiorBottom;
+
+        /** Mark border color. */
         public Color markBorder;
+
+        /** Mark interior color. */
         public Color markInterior;
 
+        /**
+         * Creates a new ButtonColors object.
+         *
+         * @param top            the top line color.
+         * @param left           the left edge color.
+         * @param edge           the main edge color.
+         * @param shadow         the Main shadow color.
+         * @param interiorTop    the interior top gradient color.
+         * @param interiorBottom the interior bottom gradient color.
+         * @param markBorder     the mark border color.
+         * @param markInterior   the mark interior color.
+         */
         public ButtonColors(Color top, Color left, Color edge, Color shadow, Color interiorTop, Color interiorBottom, Color markBorder,
-            Color markInterior) {
-            this.top = top;
-            this.left = left;
-            this.edge = edge;
-            this.shadow = shadow;
-            this.interiorTop = interiorTop;
+                Color markInterior) {
+            this.top            = top;
+            this.left           = left;
+            this.edge           = edge;
+            this.shadow         = shadow;
+            this.interiorTop    = interiorTop;
             this.interiorBottom = interiorBottom;
-            this.markBorder = markBorder;
-            this.markInterior = markInterior;
+            this.markBorder     = markBorder;
+            this.markInterior   = markInterior;
         }
 
     }
