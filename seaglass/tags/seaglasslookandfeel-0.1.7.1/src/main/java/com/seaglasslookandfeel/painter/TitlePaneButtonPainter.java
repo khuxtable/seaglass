eca9601323ff267c27dd7843fdfb5eb6048ebd2d
kathryn <kathryn@kathrynhuxtable.org>
2010-02-27 22:46:22 +0000

    [maven-release-plugin]  copy for tag seaglasslookandfeel-0.1.7.1

---
+++ seaglass/tags/seaglasslookandfeel-0.1.7.1/src/main/java/com/seaglasslookandfeel/painter/TitlePaneButtonPainter.java
@@ -0,0 +1,38 @@
+package com.seaglasslookandfeel.painter;
+
+import java.awt.Color;
+
+public abstract class TitlePaneButtonPainter extends AbstractRegionPainter {
+
+    protected static Color transparentColor               = new Color(0, true);
+
+    protected static Color black33                        = new Color(0x33000000, true);
+    protected static Color black66                        = new Color(0x66000000, true);
+    protected static Color black99                        = new Color(0x99000000, true);
+    protected static Color closeButtonEdgeHover           = new Color(0x9e5b0000, true);
+    protected static Color closeButtonInteriorBottomHover = new Color(0xd94645);
+    protected static Color closeButtonInteriorTopHover    = new Color(0xda5452);
+    protected static Color closeButtonInteriorTopPressed  = new Color(0xe8e2302d, true);
+    protected static Color closeButtonLeftHover           = new Color(0xf7a2a0);
+    protected static Color closeButtonLeftPressed         = new Color(0xf4bb6765, true);
+    protected static Color closeButtonMarkBorderHover     = new Color(0x270908);
+    protected static Color closeButtonMarkBorderPressed   = new Color(0xf4551211, true);
+    protected static Color closeButtonMarkInteriorPressed = new Color(0xfcfce9e9, true);
+    protected static Color closeButtonTopHover            = new Color(0xf39493);
+    protected static Color closeButtonTopPressed          = new Color(0xefa53634, true);
+    protected static Color gray_0e_e5                     = new Color(0xe50e0e0e, true);
+    protected static Color gray_10_e5                     = new Color(0xe5101010, true);
+    protected static Color gray_9b_82                     = new Color(0x829b9b9b, true);
+    protected static Color gray_a9_9e                     = new Color(0x9ea9a9a9, true);
+    protected static Color gray_e6                        = new Color(0xe6e6e6);
+    protected static Color gray_e6_59                     = new Color(0x59e6e6e6, true);
+    protected static Color white                          = new Color(0xffffff);
+    protected static Color white16                        = new Color(0x16ffffff, true);
+    protected static Color white33                        = new Color(0x33ffffff, true);
+    protected static Color white46                        = new Color(0x46ffffff, true);
+    protected static Color white4c                        = new Color(0x4cffffff, true);
+    protected static Color white59                        = new Color(0x59ffffff, true);
+    protected static Color white68                        = new Color(0x68ffffff, true);
+    protected static Color white8c                        = new Color(0x8cffffff, true);
+    protected static Color white99                        = new Color(0x99ffffff, true);
+}
