90731bcda62944787fae8de48ec7762a5a3d8cbc
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2010-03-09 23:39:11 +0000

    Formatted with Jalopy.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TextComponentPainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/TextComponentPainter.java
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * $Id$
  */
 package com.seaglasslookandfeel.painter;
@@ -31,31 +31,34 @@ import com.seaglasslookandfeel.effect.SeaGlassInternalShadowEffect;
  * TextComponentPainter implementation.
  */
 public final class TextComponentPainter extends AbstractCommonColorsPainter {
+
+    /**
+     * Control state.
+     */
     public static enum Which {
-        BACKGROUND_DISABLED,
-        BACKGROUND_ENABLED,
-        BACKGROUND_SOLID_DISABLED,
-        BACKGROUND_SOLID_ENABLED,
-        BACKGROUND_SELECTED,
-        BORDER_DISABLED,
-        BORDER_FOCUSED,
-        BORDER_ENABLED,
+        BACKGROUND_DISABLED, BACKGROUND_ENABLED, BACKGROUND_SOLID_DISABLED, BACKGROUND_SOLID_ENABLED, BACKGROUND_SELECTED, BORDER_DISABLED,
+        BORDER_FOCUSED, BORDER_ENABLED,
     }
 
     private SeaGlassInternalShadowEffect internalShadow = new SeaGlassInternalShadowEffect();
 
-    private Which                        state;
-    private PaintContext                 ctx;
-    private CommonControlState            type;
-    private boolean                      focused;
+    private Which              state;
+    private PaintContext       ctx;
+    private CommonControlState type;
+    private boolean            focused;
 
+    /**
+     * Creates a new TextComponentPainter object.
+     *
+     * @param state the control state to paint.
+     */
     public TextComponentPainter(Which state) {
         super();
         this.state = state;
-        this.ctx = new PaintContext(AbstractRegionPainter.PaintContext.CacheMode.FIXED_SIZES);
+        this.ctx   = new PaintContext(AbstractRegionPainter.PaintContext.CacheMode.FIXED_SIZES);
 
-        type = (state == Which.BACKGROUND_DISABLED || state == Which.BACKGROUND_SOLID_DISABLED || state == Which.BORDER_DISABLED) ? CommonControlState.DISABLED
-                : CommonControlState.ENABLED;
+        type    = (state == Which.BACKGROUND_DISABLED || state == Which.BACKGROUND_SOLID_DISABLED || state == Which.BORDER_DISABLED)
+            ? CommonControlState.DISABLED : CommonControlState.ENABLED;
         focused = (state == Which.BORDER_FOCUSED);
     }
 
@@ -65,19 +68,23 @@ public final class TextComponentPainter extends AbstractCommonColorsPainter {
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
         int x = focusInsets.left;
         int y = focusInsets.top;
-        width -= focusInsets.left + focusInsets.right;
+
+        width  -= focusInsets.left + focusInsets.right;
         height -= focusInsets.top + focusInsets.bottom;
 
         switch (state) {
+
         case BACKGROUND_DISABLED:
         case BACKGROUND_ENABLED:
         case BACKGROUND_SELECTED:
             paintBackground(g, c, x, y, width, height);
             break;
+
         case BACKGROUND_SOLID_DISABLED:
         case BACKGROUND_SOLID_ENABLED:
             paintBackgroundSolid(g, c, x, y, width, height);
             break;
+
         case BORDER_DISABLED:
         case BORDER_ENABLED:
         case BORDER_FOCUSED:
@@ -93,31 +100,65 @@ public final class TextComponentPainter extends AbstractCommonColorsPainter {
         return ctx;
     }
 
+    /**
+     * Paint the background of an editable control.
+     *
+     * @param g      DOCUMENT ME!
+     * @param c      DOCUMENT ME!
+     * @param x      DOCUMENT ME!
+     * @param y      DOCUMENT ME!
+     * @param width  DOCUMENT ME!
+     * @param height DOCUMENT ME!
+     */
     private void paintBackground(Graphics2D g, JComponent c, int x, int y, int width, int height) {
         Color color = c.getBackground();
+
         if (type == CommonControlState.DISABLED) {
             color = new Color(color.getRed(), color.getGreen(), color.getBlue(), 0x80);
         }
 
         Shape s = shapeGenerator.createRectangle(x + 1, y + 1, width - 2, height - 2);
+
         g.setPaint(color);
         g.fill(s);
     }
 
+    /**
+     * Paint the background of an uneditable control, e.g. a JLabel.
+     *
+     * @param g      DOCUMENT ME!
+     * @param c      DOCUMENT ME!
+     * @param x      DOCUMENT ME!
+     * @param y      DOCUMENT ME!
+     * @param width  DOCUMENT ME!
+     * @param height DOCUMENT ME!
+     */
     private void paintBackgroundSolid(Graphics2D g, JComponent c, int x, int y, int width, int height) {
         Color color = c.getBackground();
+
         if (type == CommonControlState.DISABLED) {
             color = new Color(color.getRed(), color.getGreen(), color.getBlue(), 0x80);
         }
 
         Shape s = shapeGenerator.createRectangle(x - 2, y - 2, width + 4, height + 4);
+
         g.setPaint(color);
         g.fill(s);
     }
 
+    /**
+     * Paint the border.
+     *
+     * @param g      DOCUMENT ME!
+     * @param c      DOCUMENT ME!
+     * @param x      DOCUMENT ME!
+     * @param y      DOCUMENT ME!
+     * @param width  DOCUMENT ME!
+     * @param height DOCUMENT ME!
+     */
     private void paintBorder(Graphics2D g, JComponent c, int x, int y, int width, int height) {
         boolean useToolBarColors = isInToolBar(c);
-        Shape s;
+        Shape   s;
 
         if (focused) {
             s = shapeGenerator.createRectangle(x - 2, y - 2, width + 3, height + 3);
