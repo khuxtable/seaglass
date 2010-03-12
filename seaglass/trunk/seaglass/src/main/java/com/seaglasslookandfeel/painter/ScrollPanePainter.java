e4afd288b365c113ed9dcee81e8cd50e214b28e1
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2010-03-12 22:40:20 +0000

    Use regular focus control for scroll pane instead of createFillableFocus.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/ScrollPanePainter.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/painter/ScrollPanePainter.java
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * $Id$
  */
 package com.seaglasslookandfeel.painter;
@@ -27,12 +27,20 @@ import java.awt.geom.Rectangle2D;
 
 import javax.swing.JComponent;
 
+import com.seaglasslookandfeel.painter.AbstractRegionPainter.FocusType;
 import com.seaglasslookandfeel.painter.AbstractRegionPainter.PaintContext.CacheMode;
 
 /**
  * Nimbus's ScrollPanePainter.
  */
 public final class ScrollPanePainter extends AbstractRegionPainter {
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @author  $author$
+     * @version $Revision$, $Date$
+     */
     public static enum Which {
         BACKGROUND_ENABLED, BORDER_ENABLED, BORDER_ENABLED_FOCUSED, CORNER_ENABLED,
     }
@@ -40,37 +48,59 @@ public final class ScrollPanePainter extends AbstractRegionPainter {
     private Which        state;
     private PaintContext ctx;
 
-    private Color        borderColor  = decodeColor("nimbusBorder");
-    private Color        focusColor   = decodeColor("nimbusFocus");
+    private Color borderColor = decodeColor("nimbusBorder");
+    private Color focusColor  = decodeColor("nimbusFocus");
 
-    private Color        cornerBorder = new Color(192, 192, 192);
-    private Color        cornerColor1 = new Color(240, 240, 240);
-    private Color        cornerColor2 = new Color(212, 212, 212);
+    private Color cornerBorder = new Color(192, 192, 192);
+    private Color cornerColor1 = new Color(240, 240, 240);
+    private Color cornerColor2 = new Color(212, 212, 212);
 
+    /**
+     * Creates a new ScrollPanePainter object.
+     *
+     * @param state DOCUMENT ME!
+     */
     public ScrollPanePainter(Which state) {
         super();
         this.state = state;
-        this.ctx = new PaintContext(CacheMode.FIXED_SIZES);
+        this.ctx   = new PaintContext(CacheMode.FIXED_SIZES);
     }
 
+    /**
+     * @see com.seaglasslookandfeel.painter.AbstractRegionPainter#doPaint(java.awt.Graphics2D,
+     *      javax.swing.JComponent, int, int, java.lang.Object[])
+     */
     protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
         switch (state) {
+
         case BORDER_ENABLED:
             paintBorderEnabled(g, width, height);
             break;
+
         case BORDER_ENABLED_FOCUSED:
             paintBorderFocused(g, width, height);
             break;
+
         case CORNER_ENABLED:
             paintCornerEnabled(g, width, height);
             break;
         }
     }
 
-    protected final PaintContext getPaintContext() {
+    /**
+     * @see com.seaglasslookandfeel.painter.AbstractRegionPainter#getPaintContext()
+     */
+    protected PaintContext getPaintContext() {
         return ctx;
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @param g      DOCUMENT ME!
+     * @param width  DOCUMENT ME!
+     * @param height DOCUMENT ME!
+     */
     private void paintBorderEnabled(Graphics2D g, int width, int height) {
         g.setPaint(borderColor);
         g.drawLine(3, 2, width - 4, 2);
@@ -79,16 +109,35 @@ public final class ScrollPanePainter extends AbstractRegionPainter {
         g.drawLine(3, height - 3, width - 4, height - 3);
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @param g      DOCUMENT ME!
+     * @param width  DOCUMENT ME!
+     * @param height DOCUMENT ME!
+     */
     private void paintBorderFocused(Graphics2D g, int width, int height) {
         paintBorderEnabled(g, width, height);
 
-        Shape s = shapeGenerator.createFillableFocusRectangle(2, 2, width - 4, height - 4);
-        g.setPaint(focusColor);
-        g.fill(s);
+        Shape s = shapeGenerator.createRectangle(0, 0, width - 1, height - 1);
+
+        g.setPaint(getFocusPaint(s, FocusType.OUTER_FOCUS, false));
+        g.draw(s);
+        s = shapeGenerator.createRectangle(1, 1, width - 3, height - 3);
+        g.setPaint(getFocusPaint(s, FocusType.INNER_FOCUS, false));
+        g.draw(s);
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @param g      DOCUMENT ME!
+     * @param width  DOCUMENT ME!
+     * @param height DOCUMENT ME!
+     */
     private void paintCornerEnabled(Graphics2D g, int width, int height) {
         Shape s = decodeCornerBorder(width, height);
+
         g.setPaint(cornerBorder);
         g.fill(s);
         s = decodeCornerInside(width, height);
@@ -96,18 +145,42 @@ public final class ScrollPanePainter extends AbstractRegionPainter {
         g.fill(s);
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  width  DOCUMENT ME!
+     * @param  height DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
     private Shape decodeCornerBorder(int width, int height) {
         return shapeGenerator.createRectangle(0, 0, width, height);
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  width  DOCUMENT ME!
+     * @param  height DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
     private Shape decodeCornerInside(int width, int height) {
         return shapeGenerator.createRectangle(1, 1, width - 2, height - 2);
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  s DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
     private Paint decodeCornerGradient(Shape s) {
         Rectangle2D bounds = s.getBounds2D();
-        float w = (float) bounds.getWidth();
-        float h = (float) bounds.getHeight();
+        float       w      = (float) bounds.getWidth();
+        float       h      = (float) bounds.getHeight();
+
         return createGradient(1, 1, w - 2, h - 2, new float[] { 0f, 1f }, new Color[] { cornerColor1, cornerColor2 });
     }
 }
