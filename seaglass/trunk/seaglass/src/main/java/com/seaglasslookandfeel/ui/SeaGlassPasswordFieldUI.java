39d2d1167ca62ee0c065945e193cb0f988bd6978
kathryn <kathryn@kathrynhuxtable.org>
2010-03-12 21:07:06 +0000

    Formatted with Jalopy.

--- seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/ui/SeaGlassPasswordFieldUI.java
+++ seaglass/trunk/seaglass/src/main/java/com/seaglasslookandfeel/ui/SeaGlassPasswordFieldUI.java
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * $Id$
  */
 package com.seaglasslookandfeel.ui;
@@ -36,17 +36,17 @@ import com.seaglasslookandfeel.SeaGlassContext;
 
 /**
  * Sea Glass PasswordField UI delegate.
- * 
- * Based on SynthPasswordTextFieldUI, but needs to extend SeaGlassTextFieldUI
- * instead.
+ *
+ * <p>Based on SynthPasswordTextFieldUI, but needs to extend SeaGlassTextFieldUI
+ * instead.</p>
  */
 public class SeaGlassPasswordFieldUI extends SeaGlassTextFieldUI {
 
     /**
      * Creates a UI for a JPasswordField.
-     * 
-     * @param c
-     *            the JPasswordField
+     *
+     * @param  c the JPasswordField
+     *
      * @return the UI
      */
     public static ComponentUI createUI(JComponent c) {
@@ -56,7 +56,7 @@ public class SeaGlassPasswordFieldUI extends SeaGlassTextFieldUI {
     /**
      * Fetches the name used as a key to look up properties through the
      * UIManager. This is used as a prefix to all the standard text properties.
-     * 
+     *
      * @return the name ("PasswordField")
      */
     protected String getPropertyPrefix() {
@@ -65,28 +65,41 @@ public class SeaGlassPasswordFieldUI extends SeaGlassTextFieldUI {
 
     /**
      * Creates a view (PasswordView) for an element.
-     * 
-     * @param elem
-     *            the element
+     *
+     * @param  elem the element
+     *
      * @return the view
      */
     public View create(Element elem) {
         return new PasswordView(elem);
     }
 
+    /**
+     * @see com.seaglasslookandfeel.ui.SeaGlassTextFieldUI#paintBackground(com.seaglasslookandfeel.SeaGlassContext,
+     *      java.awt.Graphics, javax.swing.JComponent)
+     */
     void paintBackground(SeaGlassContext context, Graphics g, JComponent c) {
         context.getPainter().paintPasswordFieldBackground(context, g, 0, 0, c.getWidth(), c.getHeight());
     }
 
+    /**
+     * @see com.seaglasslookandfeel.ui.SeaGlassTextFieldUI#paintBorder(javax.swing.plaf.synth.SynthContext,
+     *      java.awt.Graphics, int, int, int, int)
+     */
     public void paintBorder(SynthContext context, Graphics g, int x, int y, int w, int h) {
         ((SeaGlassContext) context).getPainter().paintPasswordFieldBorder(context, g, x, y, w, h);
     }
 
+    /**
+     * @see javax.swing.plaf.basic.BasicTextUI#installKeyboardActions()
+     */
     protected void installKeyboardActions() {
         super.installKeyboardActions();
         ActionMap map = SwingUtilities.getUIActionMap(getComponent());
+
         if (map != null && map.get(DefaultEditorKit.selectWordAction) != null) {
             Action a = map.get(DefaultEditorKit.selectLineAction);
+
             if (a != null) {
                 map.put(DefaultEditorKit.selectWordAction, a);
             }
