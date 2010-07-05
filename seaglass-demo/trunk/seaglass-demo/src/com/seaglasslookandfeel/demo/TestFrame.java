7f4de2b48a772bf7a17f87cb741e38a96d832eae
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2010-07-05 13:34:41 +0000

    Updated to display Chinese characters in title and sample button. Disclaimer: These characters were supplied by someone else and I have no idea what they say.

--- seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestFrame.java
+++ seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestFrame.java
@@ -1,30 +1,42 @@
 package com.seaglasslookandfeel.demo;
+
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
+/**
+ * Test frame rendering and also Chinese characters. I wonder what they mean?
+ */
 public class TestFrame extends JFrame {
+
+    /**
+     * Creates a new TestFrame object.
+     */
     public TestFrame() {
         super("TestFrame");
-        
+        setTitle("简单例子");
+
         setLayout(new FlowLayout(FlowLayout.CENTER));
-        add(new JButton("sample"));
-        
+        add(new JButton("简单例子"));
+
         setSize(400, 300);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
+    /**
+     * Main method.
+     *
+     * @param args Command line args.
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new TestFrame().setVisible(true);
-            }
-        });
+                public void run() {
+                    new TestFrame().setVisible(true);
+                }
+            });
     }
 
 }
