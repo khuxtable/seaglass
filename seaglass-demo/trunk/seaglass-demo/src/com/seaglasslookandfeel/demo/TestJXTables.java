c00037427ab5935973479403e4dea4a632b3a85d
kathryn@kathrynhuxtable.org <kathryn@kathrynhuxtable.org@1aceba94-a556-11de-bf1c-ef771b8a44ac>
2010-07-08 18:10:17 +0000

    Use static methods to force installation of Sea Glass table cell renderers.

--- seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestJXTables.java
+++ seaglass-demo/trunk/seaglass-demo/src/com/seaglasslookandfeel/demo/TestJXTables.java
@@ -1,5 +1,7 @@
 package com.seaglasslookandfeel.demo;
 
+import org.jdesktop.swingx.JXTable;
+
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
@@ -11,8 +13,6 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
 
-import org.jdesktop.swingx.JXTable;
-
 import com.seaglasslookandfeel.ui.SeaGlassTableUI;
 
 /**
@@ -22,65 +22,67 @@ public class TestJXTables {
 
     /**
      * DOCUMENT ME!
-     * 
-     * @param args
-     *            DOCUMENT ME!
+     *
+     * @param args DOCUMENT ME!
      */
     public static void main(String[] args) {
-        if (true) try {
-            UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        if (true)
+            try {
+                UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Object[][] data = new Object[][] {
-                    { "All These Things I Hate (Revolve Around Me)", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
-                    { "Cries In Vain", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
-                    { "The End", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
-                    { "Her Voice Resides", "Bullet For My Valentine", "The Poison", Boolean.TRUE },
-                    { "Hit The Floor", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
-                    { "Intro", "Bullet For My Valentine Apocalyptica", "The Poison", Boolean.FALSE },
-                    { "The Poison", "Bullet For My Valentine", "The Poison", Boolean.TRUE },
-                    { "Room 409", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
-                    { "Spit You Out", "Bullet For My Valentine", "The Poison", Boolean.TRUE },
-                    { "Suffocating Under Words Of Sorrow (What Can I Do)", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
-                    { "Tears Don't Fall", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
-                    { "4 Words (To Choke Upon)", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
-                    { "10 Years Today", "Bullet For My Valentine", "The Poison", Boolean.FALSE } };
+                public void run() {
+                    Object[][] data = new Object[][] {
+                        { "All These Things I Hate (Revolve Around Me)", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
+                        { "Cries In Vain", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
+                        { "The End", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
+                        { "Her Voice Resides", "Bullet For My Valentine", "The Poison", Boolean.TRUE },
+                        { "Hit The Floor", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
+                        { "Intro", "Bullet For My Valentine Apocalyptica", "The Poison", Boolean.FALSE },
+                        { "The Poison", "Bullet For My Valentine", "The Poison", Boolean.TRUE },
+                        { "Room 409", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
+                        { "Spit You Out", "Bullet For My Valentine", "The Poison", Boolean.TRUE },
+                        { "Suffocating Under Words Of Sorrow (What Can I Do)", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
+                        { "Tears Don't Fall", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
+                        { "4 Words (To Choke Upon)", "Bullet For My Valentine", "The Poison", Boolean.FALSE },
+                        { "10 Years Today", "Bullet For My Valentine", "The Poison", Boolean.FALSE }
+                    };
 
-                JXTable table = new JXTable(new DefaultTableModel(data, new String[] { "Name", "Artist", "Album", "Checked" })) {
-                    @Override
-                    public Class<?> getColumnClass(int column) {
-                        if (convertColumnIndexToModel(column) == 3) {
-                            return Boolean.class;
-                        } else {
-                            return Object.class;
+                    JXTable table = new JXTable(new DefaultTableModel(data, new String[] { "Name", "Artist", "Album", "Checked" })) {
+                        @Override
+                        public Class<?> getColumnClass(int column) {
+                            if (convertColumnIndexToModel(column) == 3) {
+                                return Boolean.class;
+                            } else {
+                                return Object.class;
+                            }
                         }
-                    }
-                };
-                SeaGlassTableUI ui = (SeaGlassTableUI) table.getUI();
-                // Override JXTables renderers with the Sea Glass ones.
-                ui.forceInstallRenderer(Boolean.class);
-                ui.forceInstallRenderer(Object.class);
-                table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-                table.setColumnControlVisible(true);
-                table.getColumnModel().getColumn(0).setPreferredWidth(75);
-                table.getColumnModel().getColumn(1).setPreferredWidth(50);
-                TableModel tableModel = table.getModel();
+                    };
 
-                JScrollPane scrollPane = new JScrollPane(table);
+                    // Override JXTables renderers with the Sea Glass ones.
+                    SeaGlassTableUI.forceInstallRenderer(table, Boolean.class);
+                    SeaGlassTableUI.forceInstallRenderer(table, Object.class);
 
-                JFrame frame = new JFrame();
+                    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+                    table.setColumnControlVisible(true);
+                    table.getColumnModel().getColumn(0).setPreferredWidth(75);
+                    table.getColumnModel().getColumn(1).setPreferredWidth(50);
+                    TableModel tableModel = table.getModel();
 
-                frame.add(scrollPane, BorderLayout.CENTER);
-                frame.setSize(275, 125);
-                frame.setLocationRelativeTo(null);
-                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                frame.setVisible(true);
-            }
-        });
+                    JScrollPane scrollPane = new JScrollPane(table);
+
+                    JFrame frame = new JFrame();
+
+                    frame.add(scrollPane, BorderLayout.CENTER);
+                    frame.setSize(275, 125);
+                    frame.setLocationRelativeTo(null);
+                    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    frame.setVisible(true);
+                }
+            });
     }
 
 }
